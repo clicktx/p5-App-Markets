87bc195b771bf2d82b8d8bef45f1e032374bd8ad
clicktx <clicktx@gmail.com>
2016-04-18 20:47:44 +0900

    pod

--- lib/Markets.pm
+++ lib/Markets.pm
@@ -3,3 +3,16 @@ use Mojo::Base 'Mojolicious';
 our $VERSION = '0.01';
 
 1;
+__END__
+
+=head1 NAME
+
+Markets - Markets
+
+=head1 DESCRIPTION
+
+This is a main context class for Markets
+
+=head1 AUTHOR
+
+Markets authors.
