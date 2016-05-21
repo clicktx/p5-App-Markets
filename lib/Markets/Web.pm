f60d5ac1f0c14d588481c933000c87bd563add87
clicktx <clicktx@gmail.com>
2016-05-21 17:50:59 +0900

    implement load config file

--- lib/Markets/Web.pm
+++ lib/Markets/Web.pm
@@ -2,10 +2,29 @@ package Markets::Web;
 use Mojo::Base 'Markets';
 use Data::Dumper;
 
+has config_file => sub {
+    my $self = shift;
+    my $env = $ENV{MOJO_MODE} // 'develop';
+    return "${env}.conf";
+};
+
+sub initialize_app {
+    my $self = shift;
+
+    # connect to DataBase
+    $self->plugin( Config => { file => 'etc/' . $self->config_file } );
+
+    # config from DataBase
+    $self->config( { app_config => 'from_db' } );
+}
+
 # This method will run once at server start
 sub startup {
     my $self = shift;
     my $app  = $self->app;
+
+    $self->initialize_app;
+    say Dumper $self->config; 
 
     # templets paths
     my $themes = $app->util->list_themes('theme');
