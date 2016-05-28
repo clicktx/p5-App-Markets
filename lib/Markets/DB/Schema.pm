2afb0a45e77cda18341747e830308a38b88fdbab
clicktx <clicktx@gmail.com>
2016-05-28 18:55:55 +0900

    implement load method, Teng::Schema::Loader

--- lib/Markets/DB/Schema.pm
+++ lib/Markets/DB/Schema.pm
@@ -2,4 +2,14 @@ package Markets::DB::Schema;
 use Mojo::Base -base;
 use Teng::Schema::Loader;
 
+sub load {
+    my $class = shift;
+    my %args = @_ == 1 ? %{ $_[0] } : @_;
+
+    Teng::Schema::Loader->load(
+        dbh       => $args{dbh},
+        namespace => $args{namespace},
+    );
+}
+
 1;
