2cb0fcb2f44656ea601a0aa9cda707acb29a97fd
clicktx <clicktx@gmail.com>
2016-06-26 15:09:28 +0900

    using Markets::Event

--- addons/MyAddon/lib/MyAddon.pm
+++ addons/MyAddon/lib/MyAddon.pm
@@ -7,23 +7,36 @@ sub register {
     my ( $self, $app ) = @_;
 
     $app->helper( my_addon => sub { "MyAddon, Say!" } );
-    $app->hook(
-        before_welcome => sub {
-            my $c = shift;
-            say "hook, before_welcome.";
+
+    # use prefilter_transform
+    $app->add_filter(
+        prefilter_transform => sub {
+            my ( $c, $path, $template ) = @_;
+            # say $c, $path, $template;
+        },
+        # option
+        {
+            priority => 300,      #default 100
+            config   => 'aaa',
+        }
+    );
+    $app->add_filter(
+        prefilter_transform => sub {
+            my ( $c, $path, $template ) = @_;
+            # say $c, $path, $template;
+        },
+        # option
+        {
+            priority => 10,      #default 100
+            config   => 'aaa',
         }
     );
 
-    $app->hook(
+    $app->add_filter(
         prefilter_transform => sub {
             my ( $c, $path, $template ) = @_;
             say "prefilter_transform.";
-            # say $c;
-            # say $path;
 
-            # say Dumper $template;
-
-            # say Dumper $mt, $template;
             if ( $path =~ m|admin/index/welcome| ) {
                 say "template is admin/index/welcome +++++++++++++++";
 
@@ -33,6 +46,7 @@ sub register {
 
                 # helper $app->dom
                 my $dom = $c->app->dom->parse( ${$template} );
+
                 # say "start ================================>  Markets::DOM";
                 # say $dom;
                 # say "end <================================  Markets::DOM";
@@ -59,14 +73,15 @@ sub register {
         }
     );
 
+    # after_render はhtml生成後に実行されるので毎回処理が走る
   # $app->hook(
   #     after_render => sub {
   #         my ( $c, $output, $format ) = @_;
-  # 
+  #
   #         # say $c->dumper( $c->tx );
-  # 
+  #
   #         say 'route: ' . $c->stash('controller') . '#' . $c->stash('action');
-  # 
+  #
   #         my $dom = Mojo::DOM->new( ${$output} );
   #         $dom->find('h2')->first->replace('<h2>MyAddon Mojolicious</h2>');
   #         my $h2 = $dom->at('#admin-front')->text;
