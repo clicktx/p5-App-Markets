75e329115bc2d8019c2ff03fd740328a85dfc3fc
clicktx <clicktx@gmail.com>
2016-06-29 22:07:00 +0900

    fixed variable name code

--- lib/Markets/Hook/Filter.pm
+++ lib/Markets/Hook/Filter.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Markets::Hook';
 
 sub add_filter {
     my ( $ev, $name, $code, $arg ) = ( shift, shift, shift, shift // {} );
-    $arg->{code} = $code;
+    $arg->{cb} = $code;
     $arg->{priority} //= 100;
     $ev->on( $name => $arg );
     $ev->sort($name);
