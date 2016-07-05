84d60e3ab2c62ab5dd0ea8ff03162a3216caaa8d
clicktx <clicktx@gmail.com>
2016-07-05 18:02:27 +0900

    clean up

--- lib/Markets/Addon.pm
+++ lib/Markets/Addon.pm
@@ -4,25 +4,23 @@ use Class::Inspector;
 
 sub register {
     my ( $self, $app, $conf ) = @_;
-    say "  added addon from Markets::Addon::register()";
-
-    my $addon_namespace = ref $self;
-    my $functions       = Class::Inspector->functions($addon_namespace);
+    my $namespace = ref $self;
+    my $functions = Class::Inspector->functions($namespace);
 
     foreach my $function ( @{$functions} ) {
         if ( $function =~ /^before_.+|^after_.+/ ) {
             $app->add_filter(
-                $function => \&{"$addon_namespace::$function"},
+                $function => \&{"$namespace::$function"},
                 { priority => $conf->{$function} }
             );
         }
     }
 }
 
-sub install   {}
-sub uninstall {}
-sub update    {}
-sub enable    {}
-sub disable   {}
+sub install   { }
+sub uninstall { }
+sub update    { }
+sub enable    { }
+sub disable   { }
 
 1;
