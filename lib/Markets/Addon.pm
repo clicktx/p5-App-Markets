d80727d36546392781d726f6d8ce4a796a22e800
clicktx <clicktx@gmail.com>
2016-07-05 17:57:58 +0900

    fixed remove warning message

--- lib/Markets/Addon.pm
+++ lib/Markets/Addon.pm
@@ -2,8 +2,6 @@ package Markets::Addon;
 use Mojo::Base 'Mojolicious::Plugin';
 use Class::Inspector;
 
-use Carp 'croak';
-
 sub register {
     my ( $self, $app, $conf ) = @_;
     say "  added addon from Markets::Addon::register()";
@@ -21,10 +19,10 @@ sub register {
     }
 }
 
-sub install   { croak 'Method "install" not implemented by subclass' }
-sub uninstall { croak 'Method "install" not implemented by subclass' }
-sub update    { croak 'Method "update" not implemented by subclass' }
-sub enable    { croak 'Method "enable" not implemented by subclass' }
-sub disable   { croak 'Method "disable" not implemented by subclass' }
+sub install   {}
+sub uninstall {}
+sub update    {}
+sub enable    {}
+sub disable   {}
 
 1;
