639d0c5a82ad539a9aa5ccfb3f066a535e9227cb
clicktx <clicktx@gmail.com>
2016-07-11 19:16:24 +0900

    implement loading schema and merge schema

---
+++ lib/Markets/DB/Schema/More.pm
@@ -0,0 +1,12 @@
+package Markets::DB::Schema::More;
+use strict;
+use warnings;
+use Teng::Schema::Declare;
+
+table {
+    name 'sessions';
+    pk 'sid';
+    columns qw( sid data expires );
+};
+
+1;
