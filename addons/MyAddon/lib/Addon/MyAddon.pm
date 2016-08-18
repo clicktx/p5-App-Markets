173b5b97a1da7954cd5c1e9c7a7b79902b6cd363
clicktx <clicktx@gmail.com>
2016-08-18 18:37:54 +0900

    removed register method

--- addons/MyAddon/lib/Addon/MyAddon.pm
+++ addons/MyAddon/lib/Addon/MyAddon.pm
@@ -3,29 +3,6 @@ use Mojo::Base 'Markets::Addon';
 
 use Data::Dumper;
 
-sub __register {
-    # my ( $self, $app, $conf ) = @_;
-    # say 'MyAddon regist.';
-    # my $priority = $conf->{priority};
-
-  # after_render はhtml生成後に実行されるので毎回処理が走る
-  # $app->hook(
-  #     after_render => sub {
-  #         my ( $c, $output, $format ) = @_;
-  #
-  #         # say $c->dumper( $c->tx );
-  #
-  #         say 'route: ' . $c->stash('controller') . '#' . $c->stash('action');
-  #
-  #         my $dom = Mojo::DOM->new( ${$output} );
-  #         $dom->find('h2')->first->replace('<h2>MyAddon Mojolicious</h2>');
-  #         my $h2 = $dom->at('#admin-front')->text;
-  #         $dom->at('#admin-front')->content( $h2 . 'boo' );
-  #         ${$output} = $dom;
-  #     }
-  # );
-}
-
 # 各フックポイントを関数で定義する
 sub filter_before_compile_template {
     my ( $c, $file_path, $template_source ) = @_;
