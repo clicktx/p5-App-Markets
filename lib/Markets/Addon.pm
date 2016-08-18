a1982b63545ddd827696791167628bbf56fce373
clicktx <clicktx@gmail.com>
2016-08-18 20:55:41 +0900

    renamed Markets::Plugin::Addon to Markets::Addon

--- lib/Markets/Addon.pm
+++ lib/Markets/Addon.pm
@@ -1,4 +1,33 @@
 package Markets::Addon;
-use Mojo::Base 'Markets::Plugin::Addon';
+use Mojo::Base 'Mojolicious::Plugin';
+use Class::Inspector;
+
+sub register {
+    my ( $self, $app, $conf ) = @_;
+    my $namespace = ref $self;
+    my $functions = Class::Inspector->functions($namespace);
+
+    foreach my $function ( @{$functions} ) {
+        my $module_function = $namespace . '::' . $function;
+        if ( $function =~ /^action_.+/ ) {
+            $app->add_action(
+                $function => \&{$module_function},
+                { priority => $conf->{$function} }
+            );
+        }
+        elsif ( $function =~ /^filter_.+/ ) {
+            $app->add_filter(
+                $function => \&{$module_function},
+                { priority => $conf->{$function} }
+            );
+        }
+    }
+}
+
+sub install   { }
+sub uninstall { }
+sub update    { }
+sub enable    { }
+sub disable   { }
 
 1;
