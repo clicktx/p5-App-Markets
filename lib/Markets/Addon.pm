29b53cc5e44b659804e03e660e1de1a1383307dc
clicktx <clicktx@gmail.com>
2016-08-20 13:31:01 +0900

    implement using template and DATA section

--- lib/Markets/Addon.pm
+++ lib/Markets/Addon.pm
@@ -1,6 +1,13 @@
 package Markets::Addon;
 use Mojo::Base 'Mojolicious::Plugin';
 use Class::Inspector;
+use File::Spec;
+use Mojolicious::Renderer;
+use Mojo::Util qw/slurp/;
+use constant {
+    FORMAT => 'html',
+    HANDLER => 'ep',
+};
 
 sub register {
     my ( $self, $app, $conf ) = @_;
@@ -29,5 +36,76 @@ sub uninstall { }
 sub update    { }
 sub enable    { }
 sub disable   { }
+
+sub get_template {
+    my ( $class, $template ) = @_;
+    return 'Not argument, template path' unless $template;
+
+    # Addon templates directory
+    my $home = addon_home($class);
+    my $templates_dir = File::Spec->catdir( $home, 'templates' );
+
+    my $renderer = Mojolicious::Renderer->new;
+    push @{ $renderer->classes }, $class;
+    push @{ $renderer->paths },   $templates_dir;
+
+    my $options = {
+        template => $template,
+        format   => FORMAT,
+        handler  => HANDLER
+    };
+
+    # Try template
+    if ( defined( my $path = $renderer->template_path($options) ) ) {
+        slurp $path;
+    }
+
+    # Try DATA section
+    elsif ( defined( my $d = $renderer->get_data_template($options) ) ) { $d }
+
+    # No template
+    else { "Not found template." }
+
+}
+
+# Make addon home path
+sub addon_home { Mojo::Home->new->detect(shift) }
+
+=encoding utf8
+
+=head1 NAME
+
+Markets::Addon - Markets External plugin system
+
+=head1 SYNOPSIS
+
+  # YourAddon
+  package YourAddon;
+  use Mojo::Base 'Markets::Addon';
+
+=head1 DESCRIPTION
+
+
+
+=head1 METHODS
+
+=head2 addon_home
+
+  # /path/to/Markets/addon/YourAddon
+  &addon_home(Addon::YourAddon)
+
+Get home path for YourAddon.
+
+=head2 get_template
+
+    my $content = $class->get_template('dir/template_name');
+
+Get content for addon template file or DATA section.
+
+format C<html> and handler C<ep> onry. ex) template_name.html.ep
+
+=head1 SEE ALSO
+
+=cut
 
 1;
