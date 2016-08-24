65f6e60ec4831ee8208f1bcd2e7ce0c7aff3d086
clicktx <clicktx@gmail.com>
2016-08-24 21:14:39 +0900

    change use add_filter method

--- addons/MyAddon/lib/Markets/Addon/MyAddon.pm
+++ addons/MyAddon/lib/Markets/Addon/MyAddon.pm
@@ -6,6 +6,29 @@ use Data::Dumper;
 my $class = __PACKAGE__;
 my $home  = $class->addon_home;    # get this addon home abs path.
 
+sub register {
+    my ( $self, $app, $conf ) = @_;
+
+    # add_action('init', $cb);
+    $self->add_filter(
+        'filter_before_compile_template' => \&say_yes,
+        { default_priority => 500 }    # option
+    );
+}
+
+sub say_yes {
+    my ( $c, $file_path, $template_source ) = @_;
+    say "hook say_yes: ";
+    if ( $file_path =~ m|admin/index/welcome| ) {
+        say "match -> $file_path";
+        my $dom = $c->helpers->dom->parse( ${$template_source} );
+        $dom->find('h1')->first->replace('<h1>Say yes!</h1>');
+
+        # templateの書き換えを反映
+        ${$template_source} = $dom;
+    }
+}
+
 # 各フックポイントを関数で定義する
 
 # コンパイル前のテンプレートに適用されるhook
@@ -14,7 +37,7 @@ sub filter_before_compile_template {
     say "filter hook: before_compile_template.";
 
     if ( $file_path =~ m|admin/index/welcome| ) {
-        say "  -> $file_path";
+        say "match  -> $file_path";
 
         # テンプレートを利用
         my $content = $class->get_template('welcome');
@@ -35,11 +58,13 @@ sub filter_before_compile_template {
 sub install   { }
 sub uninstall { }
 sub update    { }
+
 sub enable {
-    my $self = shift;# my ($self, $app, $arg) = (shift, shift, shift // {});
+    my $self = shift;    # my ($self, $app, $arg) = (shift, shift, shift // {});
     $self->SUPER::enable(@_);
 }
-sub disable   {
+
+sub disable {
     my $self = shift;
     $self->SUPER::disable(@_);
 }
