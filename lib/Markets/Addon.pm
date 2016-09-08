41366e1c825a90c1ee5306d0f2f5c1df428bbc78
clicktx <clicktx@gmail.com>
2016-09-08 18:32:31 +0900

    fix typo

--- lib/Markets/Addon.pm
+++ lib/Markets/Addon.pm
@@ -35,10 +35,10 @@ sub init {
     my $app  = $self->app;
 
     # Load lexicon file.
-    my $addon_dir  = $app->config('app_defaults')->{ADDONS_DIR};
+    my $addons_dir  = $app->config('app_defaults')->{ADDONS_DIR};
     my $addon_name = $self->addon_name;
     my $locale_dir =
-      File::Spec->catdir( $app->home, $addon_dir, $addon_name, 'locale' );
+      File::Spec->catdir( $app->home, $addons_dir, $addon_name, 'locale' );
     my $text_domain = decamelize($addon_name);
     $app->lexicon(
         {
