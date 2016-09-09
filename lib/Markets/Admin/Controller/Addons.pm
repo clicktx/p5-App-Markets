30a3c3a222a4da27a09a48696e89b48b9427a0e2
clicktx <clicktx@gmail.com>
2016-09-09 09:21:36 +0900

    implement addon controll page example

--- lib/Markets/Admin/Controller/Addons.pm
+++ lib/Markets/Admin/Controller/Addons.pm
@@ -6,13 +6,45 @@ use Data::Dumper;
 sub index {
     my $self = shift;
 
-    # use model
-    $self->app->model('logic-base')->do;
-    $self->app->model('Data::Base')->do;
-
-    # Render template "example/index.html.ep" with message
+    # All addons
+    my $all_addons = $self->app->addons->get_all;
     $self->render(
-        msg => 'Admin page.' );
+        all_addons       => $all_addons,
+        installed_addons => $self->app->stash('addons'),
+    );
+}
+
+sub enable {
+    my $self   = shift;
+    my $target = $self->param('target');
+    my $addon  = $self->stash('addons')->{$target};
+
+    # stash addons を更新
+    $addon->{is_enabled} = 1;
+
+    # dbを更新
+    # hook routes有効化
+    $self->app->addons->to_enable($target);
+
+    # TODO: 本番環境ではアプリケーションの再起動が必要
+    # developモードでは反映するようにする？
+
+    $self->redirect_to('/admin/addons');
+}
+
+sub disable {
+    my $self   = shift;
+    my $target = $self->param('target');
+    my $addon  = $self->stash('addons')->{$target};
+
+    # stash addons を更新
+    $addon->{is_enabled} = 0;
+
+    # dbを更新
+    # hook routes無効化
+    $self->app->addons->to_disable($target);
+
+    $self->redirect_to('/admin/addons');
 }
 
 1;
