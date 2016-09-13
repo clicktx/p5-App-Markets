e06c76b6327a99d87644c3fb8b96da414514dcad
clicktx <clicktx@gmail.com>
2016-09-13 09:35:24 +0900

    update schema

---
+++ t/App/share/sql/insert_test_data.sql
@@ -0,0 +1,14 @@
+-- -----------------------------------------------------
+-- Addons
+-- -----------------------------------------------------
+
+INSERT  INTO `markets`.`addons` (`id`, `name`, `is_enabled`)
+VALUES  (1, 'Markets::Addon::TestAddon','1'),
+        (2, 'Markets::Addon::DisableAddon', '0');
+
+-- Addon hooks
+-- -----------------------------------------------------
+
+-- INSERT  INTO `markets`.`addon_hooks` (`id`, `addon_id`, `hook_name`, `priority`)
+-- VALUES  (NULL, '1', 'before_compile_template', '300'),
+--         (NULL, '1', 'before_xxx_action', '500');
