d12b42582940090edc7a46c7f519c31f620697c4
clicktx <clicktx@gmail.com>
2016-09-17 11:00:32 +0900

    add form example

--- lib/Markets/Web/DispatchRoutes.pm
+++ lib/Markets/Web/DispatchRoutes.pm
@@ -6,7 +6,7 @@ sub register {
     my $r = $app->routes->any()->to( namespace => 'Markets::Web::Controller' );
 
     # Normal route to controller
-    $r->get('/')->to('example#welcome');
+    $r->any('/')->to('example#welcome');
 }
 
 1;
