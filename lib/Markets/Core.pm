abc5efcbc745372a10f9eece4debeb472b77c51e
clicktx <clicktx@gmail.com>
2016-09-23 10:52:03 +0900

    change argument

--- lib/Markets/Core.pm
+++ lib/Markets/Core.pm
@@ -136,7 +136,7 @@ sub initialize_app {
             # Emit filter hook (ignore static files)
             say "hook! before_routes";    # debug
             say "... This route is dynamic" unless ( $c->stash('mojo.static') );
-            $c->app->filter->emit_filter( filter_form => $c->app )
+            $c->app->filter->emit_filter( filter_form => $c )
               unless $c->stash('mojo.static');
         }
     );
