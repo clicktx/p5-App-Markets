6807f69cc310833f79f6be1e31dbd9a33955e926
clicktx <clicktx@gmail.com>
2016-10-06 08:48:36 +0900

    implement csrf validation for POST request

--- lib/Markets/Web/DispatchRoutes.pm
+++ lib/Markets/Web/DispatchRoutes.pm
@@ -3,9 +3,25 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 sub register {
     my ( $self, $app ) = @_;
-    my $r = $app->routes->namespaces(['Markets::Web::Controller']);
+    my $r = $app->routes->namespaces( ['Markets::Web::Controller'] );
 
-    # Normal route to controller
+    # CSRF protection
+    $r = $r->under(
+        sub {
+            my $c = shift;
+            return 1 if $c->req->method ne 'POST';
+
+            return 1
+              unless $c->validation->csrf_protect->has_error('csrf_token');
+            $c->render(
+                text   => 'Bad CSRF token!',
+                status => 403,
+            );
+            return 0;
+        }
+    );
+
+    # Routes
     $r->get('/')->to('example#welcome');
     $r->get('/login')->to('login#index');
     $r->post('/login/attempt')->to('login#attempt');
