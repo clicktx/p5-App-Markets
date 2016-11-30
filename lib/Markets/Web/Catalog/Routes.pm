fbf8f727adc4219408151b675dcb599dad885771
clicktx <clicktx@gmail.com>
2016-11-30 21:27:29 +0900

    add emit hook point before_action

--- lib/Markets/Web/Catalog/Routes.pm
+++ lib/Markets/Web/Catalog/Routes.pm
@@ -5,14 +5,16 @@ sub register {
     my ( $self, $app ) = @_;
     my $r = $app->routes->namespaces( ['Markets::Web::Catalog::Controller'] );
 
+    # Emit before_action hook
+    $r = $r->under( sub { $_[0]->app->plugins->emit_hook( before_action => $_[0] ) } );
+
     # CSRF protection
     $r = $r->under(
         sub {
             my $c = shift;
-            return 1 if $c->req->method eq 'GET';
 
-            return 1
-              unless $c->validation->csrf_protect->has_error('csrf_token');
+            return 1 if $c->req->method eq 'GET';
+            return 1 unless $c->validation->csrf_protect->has_error('csrf_token');
             $c->render(
                 text   => 'Bad CSRF token!',
                 status => 403,
