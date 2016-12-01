a177bd6be2e7332fb1c80062ae6e56b284813f67
clicktx <clicktx@gmail.com>
2016-12-01 19:14:05 +0900

    tidyup

--- lib/Markets/Web/Admin/Controller/Index.pm
+++ lib/Markets/Web/Admin/Controller/Index.pm
@@ -5,14 +5,15 @@ use Data::Dumper;
 # This action will render a template
 sub welcome {
     my $self = shift;
+    say "!!! start action !!!";
 
     # use model
     $self->app->model('logic-base')->do;
     $self->app->model('Data::Base')->do;
 
     # Render template "example/welcome.html.ep" with message
-    $self->render(
-        msg => 'Admin page.' );
+    $self->render( msg => 'Admin page.' );
+    say "!!! end action !!!";
 }
 
 1;
--- lib/Markets/Web/Catalog/Controller/Example.pm
+++ lib/Markets/Web/Catalog/Controller/Example.pm
@@ -5,6 +5,7 @@ use Data::Dumper;
 # This action will render a template
 sub welcome {
     my $self = shift;
+    say "!!! start action !!!";
 
     # emit hook
     $self->app->plugins->emit_hook( before_welcome => $self );
@@ -22,8 +23,9 @@ sub welcome {
     $self->app->model('Data::Base')->do;
 
     # Render template "example/welcome.html.ep" with message
-    $self->render(
-        msg => 'Welcome to the Mojolicious real-time web framework!' );
+    $self->render( msg => 'Welcome to the Mojolicious real-time web framework!' );
+
+    say "!!! end action !!!";
 }
 
 1;
