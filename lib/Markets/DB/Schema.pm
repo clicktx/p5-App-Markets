ab5e6e27c3b92aa0f0e7505aea6bd5eed0400d89
clicktx <clicktx@gmail.com>
2017-01-25 20:43:18 +0900

    remove schema for constants

--- lib/Markets/DB/Schema.pm
+++ lib/Markets/DB/Schema.pm
@@ -4,12 +4,6 @@ use warnings;
 use Teng::Schema::Declare;
 
 table {
-    name    'constants';
-    pk      'name';
-    columns qw( name default_value value summary label position );
-};
-
-table {
     name    'preferences';
     pk      'key';
     columns qw( key value default_value summary label position );
