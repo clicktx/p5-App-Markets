2c6e492c1afda25cae2bb601d1216c87b155719a
clicktx <clicktx@gmail.com>
2017-01-31 09:48:54 +0900

    fix use Markets::Model

--- lib/Markets/Model/Logic/Base.pm
+++ lib/Markets/Model/Logic/Base.pm
@@ -1,5 +1,5 @@
 package Markets::Model::Logic::Base;
-use Mojo::Base 'MojoX::Model';
+use Mojo::Base 'Markets::Model';
 
 sub do {
     my ($self) = @_;
@@ -18,7 +18,7 @@ Markets::Model::Logic::Base
 Your model
 
     package Markets::Model::Logic::Base;
-    use Mojo::Base 'MojoX::Model';
+    use Mojo::Base 'Markets::Model';
 
     sub do {
       my ($self) = @_;
@@ -61,4 +61,4 @@ Markets authors.
 
 =head1 SEE ALSO
 
-L<Mojolicious::Plugin::Model> L<MojoX::Model>
+L<Markets::Model> L<Mojolicious::Plugin::Model> L<MojoX::Model>
