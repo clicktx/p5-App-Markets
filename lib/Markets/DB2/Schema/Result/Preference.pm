59338a773880292db7f5545e461969631e87da47
clicktx <clicktx@gmail.com>
2017-02-11 10:38:19 +0900

    improve schema

--- lib/Markets/DB2/Schema/Result/Preference.pm
+++ lib/Markets/DB2/Schema/Result/Preference.pm
@@ -4,25 +4,29 @@ use warnings;
 
 use DBIx::Class::Candy -autotable => v1;
 
-primary_column key => {
+primary_column key_name => {
     data_type => 'VARCHAR',
     size      => 50,
 };
 
 column value => { data_type => 'TEXT', };
 
-column default_value => { data_type => 'TEXT', };
+column default_value => {
+    data_type   => 'TEXT',
+    is_nullable => 1,
+};
 
 column summary => { data_type => 'TEXT', };
 
 column label => {
     data_type => 'INT',
-    size      => 11,
+    size      => 7,
 };
 
 column position => {
-    data_type => 'INT',
-    size      => 11,
+    data_type     => 'INT',
+    size          => 7,
+    default_value => 0,
 };
 
 1;
