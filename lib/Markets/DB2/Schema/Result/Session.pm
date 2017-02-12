014abb1831967d5c5b6e5e494f88599bc80dc682
clicktx <clicktx@gmail.com>
2017-02-12 14:42:37 +0900

    add schema Sessions and Carts

---
+++ lib/Markets/DB2/Schema/Result/Session.pm
@@ -0,0 +1,24 @@
+package Markets::DB2::Schema::Result::Session;
+use strict;
+use warnings;
+
+use DBIx::Class::Candy -autotable => v1;
+
+primary_column sid => {
+    data_type => 'VARCHAR',
+    size      => 50,
+};
+
+column data => { data_type => 'MEDIUMTEXT', };
+
+column cart_id => {
+    data_type   => 'VARCHAR',
+    size        => 50,
+};
+
+column expires => {
+    data_type   => 'BIGINT',
+    is_nullable => 1,
+};
+
+1;
