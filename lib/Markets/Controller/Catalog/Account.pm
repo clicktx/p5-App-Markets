e3dce0ca15cb48cee4043a934d2e1a4264532f7d
clicktx <clicktx@gmail.com>
2017-02-20 20:37:49 +0900

    improve ログイン後リダイレクトに$c->flashを使うように

--- lib/Markets/Controller/Catalog/Account.pm
+++ lib/Markets/Controller/Catalog/Account.pm
@@ -5,16 +5,18 @@ sub authorize {
     my $self = shift;
     say "authorize";    #debug
     my $referer = $self->current_route;
-    my $redirect_url = $self->url_for('RN_customer_login')->query( ref => $referer );
-    $self->redirect_to($redirect_url) and return 0 unless $self->service('customer')->is_logged_in;
+
+    $self->flash( ref => $referer );
+    $self->redirect_to( $self->url_for('RN_customer_login') ) and return 0
+      unless $self->service('customer')->is_logged_in;
     return 1;
 }
 
 sub login {
-    my $self   = shift;
-    my $params = $self->req->params;
+    my $self = shift;
 
-    $self->render( ref => $params->param('ref') );
+    $self->flash( ref => $self->flash('ref') );
+    $self->render();
 }
 
 # TODO: $session->data()を直接使わないようにする
@@ -28,13 +30,14 @@ sub login_authen {
         my $customer_id = 1;
         $self->service('customer')->login($customer_id);
 
-        my $redirect_route = $params->param('ref') || 'RN_customer_home';
+        my $redirect_route = $self->flash('ref') || 'RN_customer_home';
         return $self->redirect_to($redirect_route);
     }
     else {
         say "don't loged in.";    #debug
     }
-    $self->render( template => 'account/login', ref => $params->param('ref') );
+
+    $self->render( template => 'account/login' );
 }
 
 sub logout {
