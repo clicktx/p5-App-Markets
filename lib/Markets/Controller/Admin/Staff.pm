ec7de4b585d4df224772fc52c34e931930a1ff22
clicktx <clicktx@gmail.com>
2017-03-01 19:15:51 +0900

    implement admin logout

--- lib/Markets/Controller/Admin/Staff.pm
+++ lib/Markets/Controller/Admin/Staff.pm
@@ -1,28 +1,22 @@
 package Markets::Controller::Admin::Staff;
 use Mojo::Base 'Markets::Controller::Admin';
 
-sub authen {
-    my $self   = shift;
-    my $params = $self->req->params;
-
-    my $is_valid = $params->param('password');
-    $self->render( template => 'staff/login' ) if !$is_valid;
-
-    my $staff_id = 456;    # debug staff_id example
-    $self->service('admin-staff')->login($staff_id);
-
-    my $route = $self->flash('ref') || 'RN_admin_dashboard';
-    return $self->redirect_to($route);
-}
-
 sub authorize {
     my $self = shift;
-    say "authorize";       #debug
-    return 1 if $self->service('admin-staff')->is_logged_in;
+    say "authorize";    #debug
+    return 1 if $self->is_logged_in;
 
     $self->flash( ref => $self->current_route );
     $self->redirect_to( $self->url_for('RN_admin_login') );
     return 0;
+}
+
+sub login_authen {
+    my $self   = shift;
+    my $params = $self->req->params;
+
+    my $is_valid = $params->param('password');
+    return $is_valid ? $self->_login_accept : $self->_login_failure;
 }
 
 sub login {
@@ -32,14 +26,36 @@ sub login {
     $self->render();
 }
 
-#
-# sub logout {
-#     my $self = shift;
-#     $self->service('admin-staff')->logout;
-# }
+sub logout {
+    my $self = shift;
+    $self->service('admin-staff')->logout;
+
+    return $self->redirect_to('RN_admin_login');
+}
 
 sub profile {
     my $self = shift;
+}
+
+sub _login_accept {
+    my $self = shift;
+
+    # logging etc.
+
+    my $staff_id = 456;    # debug staff_id example
+    $self->service('admin-staff')->login($staff_id);
+
+    my $route = $self->flash('ref') || 'RN_admin_dashboard';
+    return $self->redirect_to($route);
+}
+
+sub _login_failure {
+    my $self = shift;
+
+    # logging etc.
+
+    $self->flash( ref => $self->flash('ref') );
+    $self->render( template => 'staff/login' );
 }
 
 1;
--- lib/Markets/Service/Admin/Staff.pm
+++ lib/Markets/Service/Admin/Staff.pm
@@ -1,48 +1,6 @@
 package Markets::Service::Admin::Staff;
 use Mojo::Base 'Markets::Service';
 
-# getアクセスのみ履歴として保存する
-# sub add_history {
-#     my $self = shift;
-#     my $c    = $self->controller;
-# 
-#     # 履歴を残さないルート
-#     my $route_name          = $c->current_route;
-#     my $disable_route_names = $self->app->config('history_disable_route_names');
-#     return if grep { $_ eq $route_name } @$disable_route_names;
-# 
-#     my $history = $self->load_history;
-#     my $url     = $c->req->url->to_string;
-# 
-#     # リロードじゃないか
-#     # query付きgetの扱いをどうするか？　-> 別pageとして扱う
-#     return if @$history and $history->[0] eq $url;
-# 
-#     # 再訪問の場合は最新の履歴とする
-#     @$history = grep { $_ ne $url } @$history;
-# 
-#     # 保存する最大履歴数
-#     # my $max = $self->app->pref('CUSTOMER_HISTORY_MAX');
-# 
-#     unshift @$history, $url;
-#     use DDP;
-#     say "   history is";    # debug
-#     p $history;
-#     $c->server_session->data( history => $history );
-# }
-
-sub is_logged_in {
-    my $self = shift;
-    my $c    = $self->controller;
-    $c->server_session->data('staff_id') ? 1 : 0;
-}
-
-# sub load_history {
-#     my $self = shift;
-#     my $c    = $self->controller;
-#     $c->server_session->data('history') || [ $c->cookie_session('landing_page') ];
-# }
-# 
 sub login {
     my ( $self, $staff_id ) = @_;
     return unless $staff_id;
@@ -57,13 +15,13 @@ sub login {
     say "  .. regenerate_sid: " . $sid;    #debug
     return $sid;
 }
-# 
-# sub logout {
-#     my $self = shift;
-# 
-#     my $session = $self->controller->server_session;
-#     return $self->model('account')->remove_session($session);
-# }
+
+sub logout {
+    my $self = shift;
+
+    my $session = $self->controller->server_session;
+    return $self->model('account')->remove_session($session);
+}
 
 1;
 __END__
@@ -82,22 +40,6 @@ L<Markets::Service::Admin::Staff> inherits all attributes from L<Markets::Servic
 the following new ones.
 
 =head1 METHODS
-
-=head2 C<add_history>
-
-    # $app->config('history_disable_route_names') is unsave list.
-    $c->service('admin-staff')->add_history;
-
-    Add history current URL for server session.
-    Unsave list setting in L<Markets::Routes>.
-
-=head2 C<is_logged_in>
-
-    my $bool = $c->service('admin-staff')->is_logged_in;
-
-=head2 C<load_history>
-
-    my $history = $c->service('admin-staff')->load_history;
 
 =head2 C<login>
 
