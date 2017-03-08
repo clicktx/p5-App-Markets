349e6b4c5c7d2331be1544c5963b61f749309cc9
clicktx <clicktx@gmail.com>
2017-03-08 09:14:54 +0900

    implement admin/orders example

---
+++ lib/Markets/Controller/Admin/Orders.pm
@@ -0,0 +1,33 @@
+package Markets::Controller::Admin::Orders;
+use Mojo::Base 'Markets::Controller::Admin';
+
+sub index {
+    my $self = shift;
+
+    # bad!
+    my $schema = $self->app->schema;
+    my $rs     = $schema->resultset('Order');
+
+    my $it = $rs->search( {}, { page => 1, row => 10, } );
+
+    my $orders = [
+        { id => 1, date => '2000-1-1' },
+        { id => 2, date => '2000-2-2' },
+        { id => 3, date => '2000-3-3' },
+    ];
+
+    $self->stash( it => $it, orders => $orders );
+}
+
+sub detail {
+    my $self     = shift;
+    my $order_id = $self->stash('id');
+
+    # bad!
+    my $rs = $self->app->schema->resultset('Order::Item');
+    my $it = $rs->search( { order_id => $order_id } );
+
+    $self->stash( it => $it );
+}
+
+1;
