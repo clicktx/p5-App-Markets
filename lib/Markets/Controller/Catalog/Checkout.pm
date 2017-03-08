2510c4d19b333e05357b414fa3a11ce1a2b9893c
clicktx <clicktx@gmail.com>
2017-03-08 21:48:00 +0900

    implement order complete controller

--- lib/Markets/Controller/Catalog/Checkout.pm
+++ lib/Markets/Controller/Catalog/Checkout.pm
@@ -5,4 +5,52 @@ sub index {
     my $self = shift;
 }
 
+sub complete {
+    my $self = shift;
+
+    # bad!
+    my $cart   = $self->service('cart');
+    my $schema = $self->app->schema;
+
+    # 購入商品データ作成
+    my $items = $cart->items->first->to_array;
+    use DDP;
+    p $cart->data('items');
+    p $items;
+
+    # itemsが無い場合は実行しない
+    $self->redirect_to('RN_cart') if !@{$items};
+
+    # order作成
+    # itemsへ保存(relationで自動保存？)
+    my $cb;
+    $cb = sub {
+        $schema->resultset('Order')->create(
+            {
+                items => $items
+            }
+        );
+    };
+
+    use Try::Tiny;
+    my $res;
+    try {
+        $res = $cb->();
+    }
+    catch {
+        if ( $_ =~ /Rollback failed/ ) {
+
+            # ロールバックに失敗した場合
+        }
+
+        # 何らかのエラーによりロールバックした
+        warn $@;
+    };
+
+    # cart sessionクリア
+    $cart->clear;
+
+    # redirect_to thank you page
+}
+
 1;
