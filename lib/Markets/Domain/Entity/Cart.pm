1b445152e6faa6bf07539c5a7fd6c4565a8bab47
clicktx <clicktx@gmail.com>
2017-03-21 18:52:30 +0900

    improve to_hash method

--- lib/Markets/Domain/Entity/Cart.pm
+++ lib/Markets/Domain/Entity/Cart.pm
@@ -1,10 +1,11 @@
 package Markets::Domain::Entity::Cart;
 use Mojo::Base 'Markets::Domain::Entity';
 
-has [qw/ cart_id items shipments /];
+has [qw/ cart_id shipments /];
 
 has entity_id => sub { shift->cart_id };
 
+# has 'items';
 # has item_count       => sub { shift->items->flatten->size };
 # has original_total_price => 0;
 # has total_price          => 0;
@@ -22,16 +23,24 @@ use DDP;
 sub to_hash {
     my $self = shift;
 
+    my @shipments;
     my @items;
     $self->shipments->each(
         sub {
-            my @line_items;
-            shift->items->each( sub { push @line_items, shift->to_hash } );
-            push @items, \@line_items;
+            my ( $shipment, $i ) = @_;
+            $i--;
+
+            # Shipments
+            my $data           = $shipment->to_hash;
+            my $shipment_items = delete $data->{items};
+            $shipments[$i] = $data;
+
+            # Items
+            $shipment_items->each( sub { push @{ $items[$i] }, shift->to_hash } );
         }
     );
 
-    return { items => \@items, };
+    return { shipments => \@shipments, items => \@items, };
 }
 
 sub total_item_count {
