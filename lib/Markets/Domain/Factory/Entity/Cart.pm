8a914949397f072d126b38d2140e885288ea8500
clicktx <clicktx@gmail.com>
2017-03-23 08:42:00 +0900

    fix use param method

--- lib/Markets/Domain/Factory/Entity/Cart.pm
+++ lib/Markets/Domain/Factory/Entity/Cart.pm
@@ -5,36 +5,25 @@ use Mojo::Collection qw/c/;
 sub construct {
     my $self = shift;
 
-    my $data  = $self->cart_data || {};
-    my $items = $data->{items}   || [];
-    my $shipments = $data->{shipments} || [ {} ];
+    my $params = $self->params;
+    my $cart_data = delete $params->{cart_data} || {};
+    my $data;
+
+    my @items;
+    $data = $cart_data->{items} || [];
+    foreach my $item ( @{$data} ) {
+        push @items, $self->app->factory( 'entity-item', $item );
+    }
+    $params->{items} = c(@items);
 
     my @shipments;
-    for ( my $i = 0 ; $i < @{$items} ; $i++ ) {
-        my %ship = %{ $shipments->[$i] };
-        $ship{items} = $self->_shipment_items( $items->[$i] );
-        push @shipments, $self->app->factory( 'entity-shipment', \%ship );
+    $data = $cart_data->{shipments} || [];
+    foreach my $shipment ( @{$data} ) {
+        push @shipments, $self->app->factory( 'entity-shipment', $shipment );
     }
+    $params->{shipments} = c(@shipments);
 
-    return $self->construct_class->new(
-        {
-            cart_id   => $self->cart_id,
-            shipments => c(@shipments),
-        }
-    );
-}
-
-sub _shipment_items {
-    my $self       = shift;
-    my $line_items = shift;
-
-    my @shipment_items;
-    foreach my $item ( @{$line_items} ) {
-        my $entity_item = $self->app->factory( 'entity-item', $item );
-        push @shipment_items, $entity_item;
-    }
-
-    return c(@shipment_items);
+    return $self->construct_class->new($params);
 }
 
 1;
