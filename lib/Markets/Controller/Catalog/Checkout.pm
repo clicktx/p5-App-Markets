ad5885fa755ff46096d39a0930194a47157703e6
clicktx <clicktx@gmail.com>
2017-04-06 18:28:35 +0900

    [WIP]checkout process

--- lib/Markets/Controller/Catalog/Checkout.pm
+++ lib/Markets/Controller/Catalog/Checkout.pm
@@ -21,9 +21,16 @@ sub address {
 sub address_validate {
     my $self = shift;
 
-    # my $params = $self->params;
-    # use DDP;
-    # p $params;
+    my $billing_address  = $self->param('billing_address');
+    my $shipping_address = $self->param('shipping_address');
+
+    $self->cart->billing_address($billing_address);
+
+    my $shipments = $self->cart->shipments;
+    $shipments->first->shipping_address($shipping_address);
+
+    # NOTE: 変更を自動検知させたい
+    $self->cart->is_modified(1);    # save cart
 
     # $self->render( template => 'checkout/address' );
     $self->redirect_to('RN_checkout_shipping');
@@ -35,15 +42,6 @@ sub shipping {
     my $cart = $self->cart;
 
     # shipping address
-    # NOTE: 実際にはaddressで作成されているはず
-    {
-        my $data = { shipping_address => 'kamiizumi' };
-        my $shipment = $self->app->factory( 'entity-shipment', %{$data} )->create;
-
-        use Markets::Domain::Collection qw/collection/;
-        $cart->shipments( collection($shipment) ) unless $cart->shipments->size;
-    }
-
     # 商品をshipmentに移動
     # cart.itemsからitemを減らす。shipment.shipping_itemsを増やす
     # 本来は数量を考慮しなくてはならない
@@ -71,10 +69,6 @@ sub shipping {
 sub shipping_validate {
     my $self = shift;
 
-    # my $params = $self->params;
-    # use DDP;
-    # p $params;
-
     # $self->render( template => 'checkout/address' );
     $self->redirect_to('RN_checkout_confirm');
 }
@@ -90,30 +84,16 @@ sub confirm {
     $self->render();
 }
 
-sub complete {
+sub complete_validate {
     my $self = shift;
 
-    # TODO: model化する
-    # itemsが無い場合は実行しない
+    # NOTE: itemsに商品がある場合 or shipping_itemsが1つも無い場合はcomplete出来ない。
     my $cart = $self->cart;
+    return $self->redirect_to('RN_cart') if $cart->count('items') or !$cart->count('all_shipping_items');
 
-    # NOTE: itemsに商品がある場合、shipping_itemsが1つも無い場合はcomplete出来ない。
-    $self->redirect_to('RN_cart') if $cart->items->size;
-
-    # $cart->count('items') or $cart->count('shipping_items')
-
-    # cartデータからorderデータ作成
-    my $order = { billing_address => 'ogikubo' };
-    my $customer_id = $self->server_session->data('customer_id');
-    $order->{customer_id} = $customer_id;
-
-    # items
-    # NOTE: 実際にはshipmentsは作成されているはず
-    # my $shipments = [ { shipping_address => 'kamiizumi' } ];
-    # $shipments->[0]->{items} = $cart_data->{items};
-    $order->{shipments} = $cart->to_data->{shipments};
-    use DDP;
-    p $order;
+    # Make order data
+    my $order = $cart->to_order_data;
+    $order->{customer_id} = $self->server_session->data('customer_id');
 
     # Store order
     my $schema = $self->app->schema;
@@ -121,10 +101,7 @@ sub complete {
 
         # Order
         $order->{order_no} = $schema->sequence('Order');
-        $schema->resultset('Order')->create($order);
-
-        # Items
-        # $schema->resultset('Order')->create($order); # itemsがbulk insert されない
+        $schema->resultset('Order')->create($order);    # NOTE: itemsはbulk insert されない
 
         # NOTE:
         # DBIx::Class::ResultSet https://metacpan.org/pod/DBIx::Class::ResultSet#populate
@@ -147,9 +124,19 @@ sub complete {
     };
 
     # cart sessionクリア
-    $cart->clear;
+    # cartクリア（再生成）
+    my $newcart = $self->factory('entity-cart', {})->create;
+    $self->cart($newcart);
+    $self->cart->is_modified(1);
+
 
     # redirect_to thank you page
+    # $self->render();
+    $self->redirect_to('RN_checkout_complete');
+}
+
+sub complete {
+    my $self = shift;
     $self->render();
 }
 
