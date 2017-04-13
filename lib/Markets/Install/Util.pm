65eab38be2d25fa8b3ca4bcfef3ca945b961d334
clicktx <clicktx@gmail.com>
2017-04-13 17:05:32 +0900

    implement load_config function

--- lib/Markets/Install/Util.pm
+++ lib/Markets/Install/Util.pm
@@ -2,10 +2,15 @@ package Markets::Install::Util;
 use Mojo::Base -strict;
 
 use Exporter 'import';
+use Mojolicious;
+use Mojo::Home;
 use Mojo::File 'path';
 use Mojo::Util 'decode';
 
 our @EXPORT_OK = (qw(insert_data));
+
+my $MOJO_HOME = Mojo::Home->new->detect('Markets');
+my $MOJO_MODE = Mojolicious->new->mode;
 
 =head1 FUNCTIONS
 
@@ -68,6 +73,32 @@ sub insert_data {
         $action = 'populate' if ref $data->{$schema_name} eq 'ARRAY';
         $schema->resultset($schema_name)->$action( $data->{$schema_name} );
     }
+}
+
+
+=over
+
+=item C<load_config>
+
+    # default: development.conf
+    my $config = load_config();
+
+    # production or development or test
+    my $config = load_config( mode => 'production' );
+
+Load config file.
+
+=back
+
+=cut
+
+sub load_config {
+    my %arg              = @_;
+    my $mode             = $arg{mode} || $MOJO_MODE;
+    my $config_file_path = path( $MOJO_HOME, 'config', $mode . '.conf' );
+    my $conf = do $config_file_path;
+    die "development.conf does not retun HashRef" if ref($conf) ne 'HASH';
+    return $conf;
 }
 
 1;
