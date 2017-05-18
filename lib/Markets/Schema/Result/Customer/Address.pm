4cfe5d4d59db78f2106430fad6e0ddec3f16f86a
clicktx <clicktx@gmail.com>
2017-05-18 18:50:47 +0900

    add unique index

--- lib/Markets/Schema/Result/Customer/Address.pm
+++ lib/Markets/Schema/Result/Customer/Address.pm
@@ -7,12 +7,6 @@ primary_column id => {
     is_auto_increment => 1,
 };
 
-column address_type => {
-    data_type   => 'CHAR',
-    size        => 4,
-    is_nullable => 0,
-};
-
 column customer_id => {
     data_type   => 'INT',
     is_nullable => 0,
@@ -22,6 +16,14 @@ column address_id => {
     data_type   => 'INT',
     is_nullable => 0,
 };
+
+column address_type => {
+    data_type   => 'CHAR',
+    size        => 4,
+    is_nullable => 0,
+};
+
+unique_constraint ui_customer_id_address_id_address_type => [qw/customer_id address_id address_type/];
 
 belongs_to address_type => 'Markets::Schema::Result::Reference::AddressType';
 
