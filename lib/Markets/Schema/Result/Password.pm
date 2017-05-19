2c35733428348787640156df3f579cc0f7a35d5e
clicktx <clicktx@gmail.com>
2017-05-19 09:11:28 +0900

    add schema password

---
+++ lib/Markets/Schema/Result/Password.pm
@@ -0,0 +1,36 @@
+package Markets::Schema::Result::Password;
+use Mojo::Base 'Markets::Schema::ResultCommon';
+use DBIx::Class::Candy -autotable => v1;
+
+primary_column id => {
+    data_type         => 'INT',
+    is_auto_increment => 1,
+};
+
+column hash => {
+    data_type   => 'VARCHAR',
+    size        => 255,
+    is_nullable => 0,
+};
+
+column created_at => {
+    data_type   => 'DATETIME',
+    is_nullable => 0,
+    timezone    => Markets::Schema->TZ,
+};
+
+column updated_at => {
+    data_type   => 'DATETIME',
+    is_nullable => 1,
+    timezone    => Markets::Schema->TZ,
+};
+
+might_have
+  customer => 'Markets::Schema::Result::Customer',
+  { 'foreign.password_id' => 'self.id' };
+
+# might_have
+#   staff => 'Markets::Schema::Result::Staff',
+#   { 'foreign.password_id' => 'self.id' };
+
+1;
