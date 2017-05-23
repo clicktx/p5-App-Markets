0067bf49c3bd9ab5b925ccdffb1a3d5f56ad0496
clicktx <clicktx@gmail.com>
2017-05-23 12:53:02 +0900

    add entity Customer

---
+++ lib/Markets/Domain/Entity/Password.pm
@@ -0,0 +1,30 @@
+package Markets::Domain::Entity::Password;
+use Markets::Domain::Entity;
+
+has [qw/id hash created_at updated_at/];
+
+1;
+__END__
+
+=head1 NAME
+
+Markets::Domain::Entity::Password
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Markets::Domain::Entity::Password> inherits all attributes from L<Markets::Domain::Entity> and implements
+the following new ones.
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Markets authors.
+
+=head1 SEE ALSO
+
+ L<Markets::Domain::Entity>
