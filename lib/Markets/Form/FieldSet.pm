5e9cb68f9855cb76976f9d124938d92941ea6ffd
clicktx <clicktx@gmail.com>
2017-06-16 12:44:40 +0900

    add pod

--- lib/Markets/Form/FieldSet.pm
+++ lib/Markets/Form/FieldSet.pm
@@ -6,6 +6,14 @@ use Markets::Form::Field;
 
 has 'legend';
 
+sub add {
+    my ( $self, $field_name ) = ( shift, shift );
+    return unless ( my $class = ref $self || $self ) && $field_name;
+
+    no strict 'refs';
+    ${"${class}::fields"}{$field_name} = Markets::Form::Field->new( name => $field_name, @_ );
+}
+
 sub each {
     my ( $self, $cb ) = @_;
     my $class = ref $self || $self;
@@ -54,14 +62,6 @@ sub import {
     monkey_patch $caller, 'has_field', sub { add( $caller, @_ ) };
 }
 
-sub add {
-    my ( $self, $field_name ) = ( shift, shift );
-    return unless ( my $class = ref $self || $self ) && $field_name;
-
-    no strict 'refs';
-    ${"${class}::fields"}{$field_name} = Markets::Form::Field->new( name => $field_name, @_ );
-}
-
 sub remove {
     my ( $self, $field_name ) = ( shift, shift );
     return unless ( my $class = ref $self || $self ) && $field_name;
@@ -91,6 +91,37 @@ Markets::Form::Field
 
 
 =head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head2 C<has_field>
+
+    has_field 'field_name' => ( type => 'text', ... );
+
+=head1 METHODS
+
+=head2 C<add>
+
+    $obj->add( 'field_name' => ( %args ) );
+
+=head2 C<each>
+
+    $obj->each( sub { say $a, $b } );
+    $obj->each( sub { my ( $field_name, $field_obj ) } );
+
+C<$b> is L<Markets::Form::Field> object.
+
+=head2 C<field>
+
+    my $field = $obj->field('field_name');
+
+=head2 C<keys>
+
+    my @keys = $obj->keys;
+
+=head2 C<remove>
+
+    $obj->remove('field_name');
 
 =head1 SEE ALSO
 
