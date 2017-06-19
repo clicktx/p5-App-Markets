5e9b9a9340dcdc6e68c39599e6e9d4e9f53be406
clicktx <clicktx@gmail.com>
2017-06-19 17:20:41 +0900

    improve render tags

--- lib/Markets/Form/Field.pm
+++ lib/Markets/Form/Field.pm
@@ -1,7 +1,30 @@
 package Markets::Form::Field;
 use Mojo::Base -base;
 
-has [qw/name type label help error_message default_value value/];
+has id => sub { $_ = shift->name; s/\./_/g; $_ };
+has [qw/field_key name type label help error_message default_value value placeholder/];
+
+# for render tags
+has [qw(color email number range search tel text url)] => sub { _input(@_) };
+has label_for => sub { _label(@_) };
+
+sub _input {
+    my $self = shift;
+
+    my %attrs = %{$self};
+    delete $attrs{$_} for qw/label/;
+
+    my $method = $self->type . '_field';
+    return sub { shift->$method( $self->name, id => $self->id, %attrs ) };
+}
+
+sub _label {
+    my $self = shift;
+    return sub {
+        my $app = shift;
+        $app->label_for( $self->id => $app->__( $self->label ) );
+    };
+}
 
 1;
 
--- lib/Markets/Form/FieldSet.pm
+++ lib/Markets/Form/FieldSet.pm
@@ -2,17 +2,18 @@ package Markets::Form::FieldSet;
 use Mojo::Base -base;
 use Mojo::Util qw/monkey_patch/;
 use Tie::IxHash;
+use Scalar::Util qw/weaken/;
 use Markets::Form::Field;
 
 has 'legend';
 has 'params';
 
-sub add {
-    my ( $self, $field_name ) = ( shift, shift );
-    return unless ( my $class = ref $self || $self ) && $field_name;
+sub append {
+    my ( $self, $field_key ) = ( shift, shift );
+    return unless ( my $class = ref $self || $self ) && $field_key;
 
     no strict 'refs';
-    ${"${class}::fields"}{$field_name} = Markets::Form::Field->new( name => $field_name, @_ );
+    ${"${class}::field_list"}{$field_key} = +{@_};
 }
 
 sub each {
@@ -22,34 +23,38 @@ sub each {
 
     no strict 'refs';
     foreach my $a ( $self->keys ) {
-        my $b = %{"${class}::fields"}{$a};
+        my $b = %{"${class}::field_list"}{$a};
         local ( *{"${caller}::a"}, *{"${caller}::b"} ) = ( \$a, \$b );
         $a->$cb($b);
     }
 }
 
 sub field {
-    my $self = shift;
+    my ( $self, $name ) = ( shift, shift );
+    my $args = @_ > 1 ? +{@_} : shift || {};
+
     my $class = ref $self || $self;
+    my $key = _field_key($name);
 
     no strict 'refs';
-    @_ ? ${"${class}::fields"}{ $_[0] } : 0;
+    my $attrs = $key ? ${"${class}::field_list"}{$key} : {};
+    return Markets::Form::Field->new( field_key => $key, name => $name, %{$args}, %{$attrs} );
 }
-
-# sub fields {
-#     my $self = shift;
-#     my $class = ref $self || $self;
-#
-#     no strict 'refs';
-#     return %{"${class}::fields"};
-# }
 
 sub keys {
     my $self = shift;
     my $class = ref $self || $self;
 
     no strict 'refs';
-    return keys %{"${class}::fields"};
+    return keys %{"${class}::field_list"};
+}
+
+sub new {
+    my $class = shift;
+    my $self  = $class->SUPER::new(@_);
+
+    weaken $self->{params};
+    return $self;
 }
 
 sub import {
@@ -59,73 +64,63 @@ sub import {
     no strict 'refs';
     no warnings 'once';
     push @{"${caller}::ISA"}, $class;
-    tie %{"${caller}::fields"}, 'Tie::IxHash';
-    monkey_patch $caller, 'has_field', sub { add( $caller, @_ ) };
+    tie %{"${caller}::field_list"}, 'Tie::IxHash';
+    monkey_patch $caller, 'has_field', sub { append( $caller, @_ ) };
 }
 
 sub remove {
-    my ( $self, $field_name ) = ( shift, shift );
-    return unless ( my $class = ref $self || $self ) && $field_name;
+    my ( $self, $field_key ) = ( shift, shift );
+    return unless ( my $class = ref $self || $self ) && $field_key;
 
     no strict 'refs';
-    delete ${"${class}::fields"}{$field_name};
-}
-
-sub _id {
-    my $str = shift;
-    my $key = $str;
-    my $id  = $str;
-    $key =~ s/\.\d\./.[]./g;
-    $id =~ s/\./_/g;
-    return ( $key, $id );
+    delete ${"${class}::field_list"}{$field_key};
 }
 
 sub render_label {
     my $self = shift;
     my $name = shift;
 
-    my ( $key, $id ) = _id($name);
-    my $field = $self->field($key);
-
-    return sub { shift->label_for( $id => $field->label, class => 'aa-class' ) };
+    $self->field($name)->label_for;
 }
 
 sub render {
     my $self = shift;
     my $name = shift;
 
-    my ( $key, $id ) = _id($name);
-    my $field = $self->field($key);
-
-    my $method;
-    $method = 'text_field';
-
-    return sub { shift->$method( $name => ( id => $id ) ) };
+    my $field = $self->field( $name, value => $self->params->param($name) );
+    my $method = $field->type || 'text';
+    $field->$method;
 }
 
-sub renderRow {
-    my $self = shift;
-    return sub {
-        my $app = shift;
+# sub renderRow {
+#     my $self = shift;
+#     return sub {
+#         my $app = shift;
+#
+#         my $form;
+#         $self->each(
+#             sub {
+#
+#                 $form .= $app->text_field( $b->name ) . "\n";
+#             }
+#         );
+#
+#         # my $tree = ['tag', 'fieldset', undef, undef, [ 'tag', 'aaa' ], [ 'tag', 'aaa' ] ];
+#         my $tree = [ 'tag', 'fieldset', undef, undef ];
+#         my $root = Mojo::ByteStream->new( Mojo::DOM::HTML::_render($tree) );
+#         my $dom  = Mojo::DOM->new($root);
+#
+#         # $dom->at('fieldset')->append_content('123')->root;
+#         $dom->at('fieldset')->append_content( "\n" . $form )->root;
+#         $dom;
+#     };
+# }
 
-        my $form;
-        $self->each(
-            sub {
+sub _field_key { $_ = shift; s/\.\d/.[]/g; $_ }
 
-                $form .= $app->text_field( $b->name ) . "\n";
-            }
-        );
-
-        # my $tree = ['tag', 'fieldset', undef, undef, [ 'tag', 'aaa' ], [ 'tag', 'aaa' ] ];
-        my $tree = [ 'tag', 'fieldset', undef, undef ];
-        my $root = Mojo::ByteStream->new( Mojo::DOM::HTML::_render($tree) );
-        my $dom  = Mojo::DOM->new($root);
-
-        # $dom->at('fieldset')->append_content('123')->root;
-        $dom->at('fieldset')->append_content( "\n" . $form )->root;
-        $dom;
-    };
-}
+# sub _id { $_ = shift; s/\./_/g; $_ }
+#
+# sub _key_id { return ( _key( $_[0] ), _id( $_[0] ) ) }
 
 1;
 
@@ -144,7 +139,7 @@ Markets::Form::Field
 
 
     # In controller
-    my $fieldset = MyForm::Field::User->new();
+    my $fieldset = MyForm::Field::User->new(%params);
 
 
 =head1 DESCRIPTION
@@ -157,28 +152,19 @@ Markets::Form::Field
 
 =head1 METHODS
 
-=head2 C<add>
+=head2 C<append>
 
-    $obj->add( 'field_name' => ( %args ) );
-
-=head2 C<each>
-
-    $obj->each( sub { say $a, $b } );
-    $obj->each( sub { my ( $field_name, $field_obj ) } );
-
-C<$b> is L<Markets::Form::Field> object.
+    $fieldset->append( 'field_name' => ( %args ) );
 
 =head2 C<field>
 
-    my $field = $obj->field('field_name');
+    my $field = $fieldset->field('field_name');
 
-=head2 C<keys>
-
-    my @keys = $obj->keys;
+Return L<Markets::Form::Field> object.
 
 =head2 C<remove>
 
-    $obj->remove('field_name');
+    $fieldset->remove('field_name');
 
 =head1 SEE ALSO
 
