200a2b45737e7f88b1c9cf72c2e72c8f326f1ad8
clicktx <clicktx@gmail.com>
2017-06-19 17:21:37 +0900

    add form type example

---
+++ lib/Markets/Form/Type/Example.pm
@@ -0,0 +1,11 @@
+package Markets::Form::Type::Example;
+use Mojo::Base -strict;
+use Markets::Form::FieldSet;
+
+has_field email => ( type => 'email', placeholder => 'use@mail.com', label => 'E-mail' );
+has_field 'item.[].id' => ( type => 'hidden', label => 'Item ID' );
+has_field 'item.[].name' => ( type => 'text', label => 'Item Name', placeholder => 'panchi' );
+has_field name => ( type => 'text' );
+has_field address => ( type => 'text' );
+
+1;
