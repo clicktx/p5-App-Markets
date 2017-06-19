faed4193baba46876b53e9bef8e378a40998a21d
clicktx <clicktx@gmail.com>
2017-06-19 20:02:09 +0900

    fix attr

--- lib/Markets/Form/Type/Example.pm
+++ lib/Markets/Form/Type/Example.pm
@@ -6,6 +6,6 @@ has_field email => ( type => 'email', placeholder => 'use@mail.com', label => 'E
 has_field 'item.[].id' => ( type => 'hidden', label => 'Item ID' );
 has_field 'item.[].name' => ( type => 'text', label => 'Item Name', placeholder => 'panchi' );
 has_field name => ( type => 'text' );
-has_field address => ( type => 'text' );
+has_field address => ( type => 'text', label => 'Address', placeholder => 'Silicon Valley' );
 
 1;
