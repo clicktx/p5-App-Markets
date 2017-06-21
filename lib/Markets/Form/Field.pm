b1665ffad0d0c1d58b4eecef202ade973b5539f9
clicktx <clicktx@gmail.com>
2017-06-21 12:12:32 +0900

    improve input tag render

--- lib/Markets/Form/Field.pm
+++ lib/Markets/Form/Field.pm
@@ -1,21 +1,41 @@
 package Markets::Form::Field;
 use Mojo::Base -base;
+use Carp qw/croak/;
 
 has id => sub { $_ = shift->name; s/\./_/g; $_ };
 has [qw/field_key name type label help error_message default_value value placeholder/];
 
-# for render tags
-has [qw(color email number range search tel text url)] => sub { _input(@_) };
-has label_for => sub { _label(@_) };
+sub AUTOLOAD {
+    my $self = shift;
+    my ( $package, $method ) = our $AUTOLOAD =~ /^(.+)::(.+)$/;
+
+    # label
+    return _label( $self, @_ ) if $method eq 'label_for';
+
+    # input
+    my %attr = %{$self};
+    $attr{id} = $self->id;
+    delete $attr{$_} for qw/field_key type label/;
+
+    my @time  = qw(datetime date month time week);
+    my @other = qw(file hidden password);
+    for my $name ( @time, @other, qw(color email number range search tel text url) ) {
+        return _input( $self, method => "${name}_field", %attr, @_ ) if $method eq $name;
+    }
+
+    # textarea
+    # select
+    # checkbox radio
+
+    Carp::croak "Undefined subroutine &${package}::$method called";
+}
 
 sub _input {
     my $self = shift;
+    my %arg  = @_;
 
-    my %attrs = %{$self};
-    delete $attrs{$_} for qw/field_key label/;
-
-    my $method = $self->type . '_field';
-    return sub { shift->$method( $self->name, id => $self->id, %attrs ) };
+    my $method = delete $arg{method};
+    return sub { shift->$method( $self->name, %arg ) };
 }
 
 sub _label {
