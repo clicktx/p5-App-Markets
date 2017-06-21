53dad61205c76aacc726bc895270dc1db32561ba
clicktx <clicktx@gmail.com>
2017-06-21 12:45:43 +0900

    fix hidden field renderer

--- lib/Markets/Form/Field.pm
+++ lib/Markets/Form/Field.pm
@@ -18,10 +18,13 @@ sub AUTOLOAD {
     delete $attr{$_} for qw/field_key type label/;
 
     my @time  = qw(datetime date month time week);
-    my @other = qw(file hidden password);
+    my @other = qw(file password);
     for my $name ( @time, @other, qw(color email number range search tel text url) ) {
         return _input( $self, method => "${name}_field", %attr, @_ ) if $method eq $name;
     }
+
+    # hidden
+    return _hidden( $self, %attr, @_ ) if $method eq 'hidden';
 
     # textarea
     # select
@@ -30,19 +33,29 @@ sub AUTOLOAD {
     Carp::croak "Undefined subroutine &${package}::$method called";
 }
 
+sub _hidden {
+    my $field = shift;
+    return sub { shift->hidden_field( $field->name, $field->value, @_ ) };
+}
+
 sub _input {
-    my $self = shift;
-    my %arg  = @_;
+    my $field = shift;
+    my %arg   = @_;
 
     my $method = delete $arg{method};
-    return sub { shift->$method( $self->name, %arg ) };
+    return sub {
+        my $app = shift;
+
+        # $arg{placeholder} = $app->__( $arg{placeholder} );
+        $app->$method( $field->name, %arg );
+    };
 }
 
 sub _label {
-    my $self = shift;
+    my $field = shift;
     return sub {
         my $app = shift;
-        $app->label_for( $self->id => $app->__( $self->label ) );
+        $app->label_for( $field->id => $app->__( $field->label ) );
     };
 }
 
