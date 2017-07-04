ba6ce1bc75194d018c509e787110bbb1210fdb42
clicktx <clicktx@gmail.com>
2017-07-04 09:24:06 +0900

    add customer_pasword_min/max

--- share/default_data.pl
+++ share/default_data.pl
@@ -8,6 +8,10 @@
         [ 'admin_uri_prefix', undef, '/admin', 'pref.title.admin_uri_prefix', 'pref.summary.admin_uri_prefix', 100, 1 ],
         [ 'addons_dir',       undef, 'addons', 'pref.title.addons_dir', 'pref.summary.addons_dir',       200, 1 ],
         [ 'shop_name', undef, 'Markets Shop', '', '', 100, 2 ],
+
+        # customer
+        [ 'customer_password_min', undef, '4', '', '', 100, 2 ],
+        [ 'customer_password_max', undef, '8', '', '', 101, 2 ],
     ],
 
     # reference table
