5783fb8bed53fcb0e35b84260320dca1cf515a8e
clicktx <clicktx@gmail.com>
2017-07-05 09:19:16 +0900

    improve doc, add SCHEMA

--- lib/Markets/Form/FieldSet.pm
+++ lib/Markets/Form/FieldSet.pm
@@ -236,6 +236,62 @@ Markets::Form::Field
         $c->render( text => 'validation failure');
     }
 
+=head1 SCHEMA
+
+    package Markets::Form::Type::Example;
+    use Mojo::Base -strict;
+    use Markets::Form::FieldSet;
+
+    has_field email => (
+        type          => 'email',
+        placeholder   => 'name@domain',
+        label         => 'E-mail',
+        default_value => 'a@b.com',
+        required      => 1,
+        filters       => [qw(trim)],
+        validations   => [],
+    );
+
+    has_field password => (
+        type        => 'password',
+        placeholder => 'your password',
+        label       => 'Password',
+        required      => 1,
+        filters     => [],
+        validations => [ { size => [ \'customer_password_min', \'customer_password_max' ] }, ],
+        help        => sub {
+            my $c = shift;
+            $c->__x(
+                'Must be {min}-{max} characters long.',
+                { min => $c->pref('customer_password_min'), max => $c->pref('customer_password_max') },
+            );
+        },
+    );
+
+    has_field password_again => (
+        type        => 'password',
+        placeholder => 'password again',
+        label       => 'Password Again',
+        required    => 1,
+        filters        => [],
+        validations    => [ { equal_to => 'password' } ],
+        help           => 'Type Password Again.',
+        error_messages => {
+            equal_to => 'The passwords you entered do not much.',
+        },
+    );
+
+=head2 C<validations>
+
+    validations => [ int, { size => [ 4, 8 ] }, ... ],
+
+Set array refference.
+If the method has arguments, it returns hash refference.
+
+    # Value from Preferences
+    validations => [ int, { size => [ \'password_min', \'password_max' ] }, ... ],
+
+Passing a scalar reffernce as an arguments to the validator method expands from preferences.
 
 =head1 DESCRIPTION
 
