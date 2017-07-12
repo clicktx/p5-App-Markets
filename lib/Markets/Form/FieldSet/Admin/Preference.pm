d53a0dec0d74a4d9748d3a40b8ec224763de7ef7
clicktx <clicktx@gmail.com>
2017-07-12 21:01:59 +0900

    add field set

---
+++ lib/Markets/Form/FieldSet/Admin/Preference.pm
@@ -0,0 +1,35 @@
+package Markets::Form::FieldSet::Admin::Preference;
+use Mojo::Base -strict;
+use Markets::Form::FieldSet;
+
+has_field 'admin_uri_prefix' => (
+    type        => 'text',
+    validations => [],
+);
+
+has_field 'addons_dir' => (
+    type        => 'text',
+    validations => [],
+);
+
+has_field 'locale_country' => (
+    type        => 'text',
+    validations => [],
+);
+
+has_field 'shop_name' => (
+    type        => 'text',
+    validations => [],
+);
+
+has_field 'customer_password_min' => (
+    type        => 'text',
+    validations => ['int'],
+);
+
+has_field 'customer_password_max' => (
+    type        => 'text',
+    validations => ['int'],
+);
+
+1;
