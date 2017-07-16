b8ada6ce19e601530c03224fcde08d801fbfd29e
clicktx <clicktx@gmail.com>
2017-07-16 15:09:15 +0900

    add schema Product

---
+++ lib/Markets/Schema/Result/Product.pm
@@ -0,0 +1,27 @@
+package Markets::Schema::Result::Product;
+use Mojo::Base 'Markets::Schema::Base::Result';
+use DBIx::Class::Candy -autotable => v1;
+
+primary_column id => {
+    data_type         => 'INT',
+    is_auto_increment => 1,
+};
+
+column title => {
+    data_type => 'VARCHAR',
+    size      => 128,
+    is_nullable => 0,
+};
+
+column description => {
+    data_type   => 'TEXT',
+    is_nullable => 0,
+};
+
+column price => {
+    data_type     => 'INT',
+    default_value => 0,
+    is_nullable   => 0,
+};
+
+1;
