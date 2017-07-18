c86a5b7fe833c9ed81c06441e83ca6a84f9eb086
clicktx <clicktx@gmail.com>
2017-07-18 19:41:30 +0900

    implement subtotal method

--- lib/Markets/Domain/Entity/Cart/Item.pm
+++ lib/Markets/Domain/Entity/Cart/Item.pm
@@ -2,8 +2,10 @@ package Markets::Domain::Entity::Cart::Item;
 use Markets::Domain::Entity;
 
 has id => sub { shift->hash_code };
-has product_id => '';
-has quantity   => 0;
+has product_id    => '';
+has product_title => '';
+has price         => 0;
+has quantity      => 0;
 
 sub hash_code {
     my $self  = shift;
@@ -11,6 +13,14 @@ sub hash_code {
 
     # $bytes .= ...;
     $self->SUPER::hash_code($bytes);
+}
+
+sub subtotal {
+    my $self     = shift;
+    my $subtotal = 0;
+
+    $subtotal = $self->price * $self->quantity;
+    return $subtotal;
 }
 
 1;
@@ -29,10 +39,27 @@ Markets::Domain::Entity::Item
 L<Markets::Domain::Entity::Item> inherits all attributes from L<Markets::Domain::Entity> and implements
 the following new ones.
 
+=head2 C<id>
+
+=head2 C<product_id>
+
+=head2 C<product_title>
+
+=head2 C<quantity>
+
+=head2 C<price>
+
 =head1 METHODS
 
 L<Markets::Domain::Entity::Item> inherits all methods from L<Markets::Domain::Entity> and implements
 the following new ones.
+
+=head2 C<subtotal>
+
+    $item->subtotal
+
+Returns the combined price of all the items in the row.
+This is equal to C< $item-E<gt>price> times C<$item-E<gt>quantity>.
 
 =head1 AUTHOR
 
