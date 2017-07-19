aa2ff12761f9e1152bb9bbb81bd48f9babb9337e
clicktx <clicktx@gmail.com>
2017-07-20 08:33:06 +0900

    implement update cart form

--- lib/Markets/Controller/Catalog/Cart.pm
+++ lib/Markets/Controller/Catalog/Cart.pm
@@ -7,6 +7,19 @@ sub index {
     my $cart = $self->service('cart')->create_entity;
     $self->stash( cart => $cart );
 
+    my $form = $self->form_set('cart');
+    $cart->items->each(
+        sub {
+            my ( $item, $i ) = @_;
+            $form->field("item.$i.quantity")->value( $item->quantity );
+        }
+    );
+
+    return $self->render() unless $form->has_data;
+
+    if   ( $form->validate ) { }
+    else                     { }
+
     $self->render();
 }
 
