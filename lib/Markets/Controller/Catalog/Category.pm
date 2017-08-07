bae99939402a879f4a23707f46aa85ea68a59542
clicktx <clicktx@gmail.com>
2017-08-07 20:02:55 +0900

    improve view category title

--- lib/Markets/Controller/Catalog/Category.pm
+++ lib/Markets/Controller/Catalog/Category.pm
@@ -3,6 +3,17 @@ use Mojo::Base 'Markets::Controller::Catalog';
 
 sub index {
     my $self = shift;
+
+    my $category_id   = $self->stash('category_id');
+    my $category_name = $self->stash('category_name');
+
+    my $rs = $self->app->schema->resultset('Category');
+    $self->stash( rs => $rs );
+
+    my $category;
+    $category = $rs->find($category_id) if $category_id;
+
+    $self->stash( title => $category->title ) if $category;
     $self->render();
 }
 
