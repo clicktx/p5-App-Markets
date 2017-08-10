3a1df6e61097beb295c4eeaab272ddca29fb447d
clicktx <clicktx@gmail.com>
2017-08-10 11:15:06 +0900

    improve controller admin/product

--- lib/Markets/Controller/Admin/Product.pm
+++ lib/Markets/Controller/Admin/Product.pm
@@ -10,6 +10,14 @@ use Mojo::Base 'Markets::Controller::Admin';
 
 sub create {
     my $self = shift;
+
+    my $form = $self->form_set('admin-product');
+    $self->init_form();
+
+    return $self->render() if !$form->has_data or !$form->validate;
+
+    my $rs = $self->schema->resultset('Product');
+
     return $self->render();
 }
 
@@ -20,11 +28,13 @@ sub edit {
     my $rs         = $self->schema->resultset('Product');
     my $product    = $rs->find($product_id);
 
+    # init form
     my $form = $self->form_set('admin-product');
-    $form->field($_)->value( $product->$_ ) for $product->columns;
+    $form->field('product_id')->required(1)->default_value($product_id);
+    $form->field($_)->default_value( $product->$_ ) for $product->columns;
     $self->init_form();
 
-    return $self->render() unless $form->has_data;
+    return $self->render() if !$form->has_data or !$form->validate;
 
     return $self->render();
 }
