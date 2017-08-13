48acba54e710ffb8752aa17b2035a72a78f0666d
clicktx <clicktx@gmail.com>
2017-08-13 10:08:21 +0900

    add column for Product::Category shcema

--- lib/Markets/Schema/Result/Product/Category.pm
+++ lib/Markets/Schema/Result/Product/Category.pm
@@ -17,6 +17,12 @@ column category_id => {
     is_nullable => 0,
 };
 
+column is_primary => {
+    data_type     => 'BOOLEAN',
+    is_nullable   => 0,
+    default_value => 0,
+};
+
 belongs_to
   product => 'Markets::Schema::Result::Product',
   { 'foreign.id' => 'self.product_id' };
