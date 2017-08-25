84124b77a15e6dcae03ecef788993dbc38419b56
clicktx <clicktx@gmail.com>
2017-08-25 18:13:18 +0900

    add service category

---
+++ lib/Markets/Service/Category.pm
@@ -0,0 +1,33 @@
+package Markets::Service::Category;
+use Mojo::Base 'Markets::Service';
+
+has resultset => sub { shift->schema->resultset('Category') };
+
+1;
+__END__
+
+=head1 NAME
+
+Markets::Service::Category
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Markets::Service::Category> inherits all attributes from L<Markets::Service> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Markets::Service::Category> inherits all methods from L<Markets::Service> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Markets authors.
+
+=head1 SEE ALSO
+
+L<Markets::Service>
