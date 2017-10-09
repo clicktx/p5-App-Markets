33d2efc0b70a57c79df67708ef71c361d1e67ce2
clicktx <clicktx@gmail.com>
2017-10-09 16:05:11 +0900

    fix routes

--- lib/Markets/Routes.pm
+++ lib/Markets/Routes.pm
@@ -72,22 +72,11 @@ sub add_admin_routes {
         {
             my $orders = $r->any('/orders')->to( controller => 'admin-orders' );
             $orders->any('/')->to('#index')->name('RN_admin_orders');
-<<<<<<< HEAD
-<<<<<<< HEAD
-            $orders->any('/detail/:id')->to('#detail')->name('RN_admin_orders_detail');
-=======
             {
                 my $detail = $orders->any('/detail/:id');
                 $detail->get('')->to('#detail')->name('RN_admin_orders_detail');
                 $detail->any('/edit')->to('#edit')->name('RN_admin_orders_detail_edit');
             }
->>>>>>> 1e00e81... fix
-=======
-            {
-                my $detail = $orders->any('/detail/:id')->to('#detail')->name('RN_admin_orders_detail');
-                $detail->any('/edit')->to('#edit')->name('RN_admin_orders_detail_edit');
-            }
->>>>>>> 47700b0... improve admin order edit page
         }
 
         # Order
