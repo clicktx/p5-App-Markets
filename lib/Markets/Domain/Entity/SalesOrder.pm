bd8dde93a390157a420012c89f52c64c9b349149
clicktx <clicktx@gmail.com>
2017-10-15 09:35:37 +0900

    improve Entity::SalesOrder, add attributes

--- lib/Markets/Domain/Entity/SalesOrder.pm
+++ lib/Markets/Domain/Entity/SalesOrder.pm
@@ -1,8 +1,14 @@
 package Markets::Domain::Entity::SalesOrder;
 use Markets::Domain::Entity;
+use Markets::Domain::Collection;
+use Markets::Domain::Entity::Customer;
+use Markets::Domain::Entity::Address;
 
-has [qw/order_number customer_id billing_address created_at updated_at/];
-has shipments => sub { Markets::Domain::Collection->new };
+has billing_address => sub { Markets::Domain::Entity::Address->new };
+has created_at      => undef;
+has customer        => sub { Markets::Domain::Entity::Customer->new };
+has shipments       => sub { Markets::Domain::Collection->new };
+has updated_at      => undef;
 
 sub is_multiple_shipment { return @{ shift->shipments } > 1 ? 1 : 0 }
 
@@ -21,6 +27,27 @@ Markets::Domain::Entity::SalesOrder
 
 L<Markets::Domain::Entity::SalesOrder> inherits all attributes from L<Markets::Domain::Entity> and implements
 the following new ones.
+
+=head2 C<billing_address>
+
+Return L<Markets::Domain::Entity::Address> object.
+
+=head2 C<created_at>
+
+Return L<DateTime> object or undef.
+
+=head2 C<customer>
+
+Return L<Markets::Domain::Entity::Customer> object.
+
+=head2 C<shipments>
+
+Return L<Markets::Domain::Collection> object.
+Elements is L<Markets::Domain::Entity::Shipment> object.
+
+=head2 C<updated_at>
+
+Return L<DateTime> object or undef.
 
 =head1 METHODS
 
