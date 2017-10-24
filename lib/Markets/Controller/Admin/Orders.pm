103bfff2e82568be0cc41472b14416fb8dc62de5
clicktx <clicktx@gmail.com>
2017-10-24 21:30:29 +0900

    add pagination at admin/orders

--- lib/Markets/Controller/Admin/Orders.pm
+++ lib/Markets/Controller/Admin/Orders.pm
@@ -4,22 +4,41 @@ use Mojo::Base 'Markets::Controller::Admin';
 sub index {
     my $self = shift;
 
+    my $form = $self->form_set('search-order');
+    $self->init_form();
+
+    # return $self->render() unless $form->has_data;
+    $form->validate;
+
+    my $page_no = $form->param('p') || 1;
+    my $rows = 5;
+
     # bad!
     my $schema = $self->app->schema;
-    my $itr    = $schema->resultset('Sales::Order::Shipment')->search(
+    my $orders = $schema->resultset('Sales::Order::Shipment')->search(
         {},
         {
-            page => 1,
-            row  => 3,
-
-            # order_by => { -desc => 'order_header_id' },
-            prefetch => { order_header => 'billing_address' },
-
-            # prefetch => [{ order_header => 'postal_address' }, 'shipping_address' ],
+            page => $page_no,
+            rows  => $rows,
+            order_by => { -desc => 'order_header_id' },
+            prefetch => [ 'shipping_address', { order_header => 'billing_address' }, ],
         }
     );
 
-    $self->stash( itr => $itr );
+    # content entity
+    my $content = $self->app->factory('entity-content')->create(
+        {
+            # title      => $xx->title,
+            # breadcrumb => $xx->breadcrumb,
+            pager  => $orders->pager,
+            params => $form->params->to_hash,
+        }
+    );
+
+    $self->stash(
+        content => $content,
+        orders  => $orders,
+    );
     $self->render();
 }
 
