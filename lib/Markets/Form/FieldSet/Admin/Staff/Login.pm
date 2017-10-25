304c1d237273fb0eeb8a20de555714c439643f51
clicktx <clicktx@gmail.com>
2017-10-25 19:59:03 +0900

    fix import form field

--- lib/Markets/Form/FieldSet/Admin/Staff/Login.pm
+++ lib/Markets/Form/FieldSet/Admin/Staff/Login.pm
@@ -1,5 +1,5 @@
 package Markets::Form::FieldSet::Admin::Staff::Login;
 use Mojo::Base -strict;
-use Markets::Form::FieldSet::Basic -export_field => [qw(login_id password)];
+use Markets::Form::FieldSet::Basic qw(login_id password);
 
 1;
