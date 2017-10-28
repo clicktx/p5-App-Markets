5f02da323b1cb2004ccf839b96698e0ec15c162f
clicktx <clicktx@gmail.com>
2017-10-29 08:06:26 +0900

    implement factory Entity::Staff::build

--- lib/Markets/Domain/Factory/Entity/Staff.pm
+++ lib/Markets/Domain/Factory/Entity/Staff.pm
@@ -1,13 +1,33 @@
 package Markets::Domain::Factory::Entity::Staff;
 use Mojo::Base 'Markets::Domain::Factory';
 
+sub build {
+    my ( $self, $arg ) = @_;
+
+    # NOTE: staff のlogin_id は数字のみを許可しないこと
+    my $where = $arg =~ /\D/ ? { 'me.login_id' => $arg } : { 'me.id' => $arg };
+    my $columns = [
+        qw(me.id me.login_id me.created_at me.updated_at),
+        qw(password.id password.hash password.created_at password.updated_at),
+    ];
+
+    my $itr = $self->app->schema->resultset('Staff')->search(
+        $where,
+        {
+            columns  => $columns,
+            prefetch => 'password',
+        }
+    );
+    my $data = $itr->hashref_first;
+    return $data ? $self->create($data) : undef;
+}
+
 sub cook {
     my $self = shift;
 
     # password
     my $password = $self->factory('entity-password')->create( $self->{password} || {} );
     $self->param( password => $password );
-
 }
 
 1;
@@ -35,6 +55,14 @@ the following new ones.
 
 L<Markets::Domain::Factory::Entity::Staff> inherits all methods from L<Markets::Domain::Factory> and implements
 the following new ones.
+
+=head2 C<build>
+
+    my $entity = $factory->build( $id | $login_id );
+
+Return L<Markets::Domain::Entity::Staff> object.
+
+Create entity by staff ID or login_id.
 
 =head1 AUTHOR
 
