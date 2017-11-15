6c9024e4002f09baa12cd83d1a7ca1b52262bb7a
clicktx <clicktx@gmail.com>
2017-11-16 06:57:03 +0900

    extract methods

--- lib/Yetie/Form/FieldSet.pm
+++ lib/Yetie/Form/FieldSet.pm
@@ -1,18 +1,9 @@
 package Yetie::Form::FieldSet;
 use Mojo::Base -base;
 use Mojo::Util qw/monkey_patch/;
-use Tie::IxHash;
-use Scalar::Util qw/weaken/;
-use Carp qw/croak/;
-use CGI::Expand qw/expand_hash/;
-use Mojolicious::Controller;
 use Mojo::Collection;
-use Yetie::Parameters;
+use Tie::IxHash;
 use Yetie::Form::Field;
-
-has controller => sub { Mojolicious::Controller->new };
-has 'is_validated';
-has validation => sub { shift->controller->validation };
 
 sub append_field {
     my ( $self, $field_key ) = ( shift, shift );
@@ -56,7 +47,7 @@ sub field {
     my $args = @_ > 1 ? +{@_} : shift || {};
     my $class = ref $self || $self;
 
-    my $field_key = _replace_key($name);
+    my $field_key = $self->_replace_key($name);
     my $cache_key = $name eq $field_key ? $field_key : "$field_key=$name";
     return $self->{_field}->{$cache_key} if $self->{_field}->{$cache_key};
 
@@ -68,8 +59,6 @@ sub field {
 }
 
 sub filters { shift->_get_data( shift, 'filters' ) }
-
-sub has_data { shift->controller->validation->has_data }
 
 sub import {
     my $class  = shift;
@@ -88,68 +77,12 @@ sub import {
     $_[0] eq '-all' ? $class->export_field($caller) : $class->export_field( $caller, @_ );
 }
 
-sub new {
-    my $class = shift;
-    my $self  = $class->SUPER::new(@_);
-
-    weaken $self->{controller};
-    return $self;
-}
-
-sub param {
-    my ( $self, $key ) = @_;
-    $key =~ m/\[\]$/ ? $self->params->every_param($key) : $self->params->param($key);
-}
-
-sub params {
-    my $self = shift;
-    croak 'do not call "validate" method' unless $self->is_validated;
-    return $self->{_validated_parameters} if $self->{_validated_parameters};
-
-    my $v      = $self->controller->validation;
-    my %output = %{ $v->output };
-
-    # NOTE: scope parameterは別に保存していないので
-    # 'user.name' フィールドを使う場合は 'user'フィールドを使うことが出来ない
-    my $expand_hash = expand_hash( \%output );
-    %output = ( %output, %{$expand_hash} );
-
-    $self->{_validated_parameters} = Yetie::Parameters->new(%output);
-    return $self->{_validated_parameters};
-}
-
 sub remove {
     my ( $self, $field_key ) = ( shift, shift );
     return unless ( my $class = ref $self || $self ) && $field_key;
 
     no strict 'refs';
     delete ${"${class}::schema"}{$field_key};
-}
-
-sub render_error {
-    my ( $self, $name ) = @_;
-    $self->field($name)->error_block( $self->controller );
-}
-
-sub render_help {
-    my ( $self, $name ) = @_;
-    $self->field($name)->help_block( $self->controller );
-}
-
-sub render_label {
-    my ( $self, $name, %attrs ) = @_;
-    $self->field($name)->label_for( $self->controller, %attrs );
-}
-
-sub render {
-    my ( $self, $name, %attrs ) = @_;
-
-    my $value = $self->controller->req->params->param($name);
-    $attrs{value} = $value if defined $value;
-
-    my $field = $self->field($name);
-    my $method = $field->type || 'text';
-    $field->$method( $self->controller, %attrs );
 }
 
 sub schema {
@@ -159,52 +92,6 @@ sub schema {
     no strict 'refs';
     my %schema = %{"${class}::schema"};
     return $field_key ? $schema{$field_key} : \%schema;
-}
-
-sub scope_param { shift->params->every_param(shift) }
-
-sub validate {
-    my $self  = shift;
-    my $v     = $self->controller->validation;
-    my $names = $self->controller->req->params->names;
-
-    foreach my $field_key ( @{ $self->field_keys } ) {
-        my $required = $self->schema->{$field_key}->{required};
-        my $filters  = $self->filters($field_key);
-        my $checks   = $self->checks($field_key);
-
-        # multiple field: eg. parameter_name = "favorite_color[]"
-        $field_key .= '[]' if $self->schema($field_key)->{multiple};
-
-        # expanding field: e.g. field_key = "user.[].id" parameter_name = "user.0.id"
-        if ( $field_key =~ m/\.\[\]/ ) {
-            my @match = grep { my $name = _replace_key($_); $field_key eq $name } @{$names};
-            foreach my $key (@match) {
-                $required ? $v->required( $key, @{$filters} ) : $v->optional( $key, @{$filters} );
-                $self->_do_check( $v, $_ ) for @$checks;
-                _replace_req_param( $self->controller, $v, $key );
-            }
-        }
-        else {
-            $required ? $v->required( $field_key, @{$filters} ) : $v->optional( $field_key, @{$filters} );
-            $self->_do_check( $v, $_ ) for @$checks;
-            _replace_req_param( $self->controller, $v, $field_key );
-        }
-    }
-    $self->is_validated(1);
-    return $v->has_error ? undef : 1;
-}
-
-sub _do_check {
-    my $self = shift;
-    my $v    = shift;
-
-    my ( $check, @args ) = ref $_[0] eq 'ARRAY' ? @{ $_[0] } : $_[0];
-    return $v->$check unless @args;
-
-    # scalar refference to preference value
-    @args = map { ref $_ eq 'SCALAR' ? $self->controller->pref( ${$_} ) : $_ } @args;
-    return $v->$check(@args);
 }
 
 sub _get_data {
@@ -219,14 +106,11 @@ sub _get_data {
     }
 }
 
-# NOTE: filter適用後の値をfill-in formで使われるようにする
-sub _replace_req_param {
-    my ( $c, $v, $key ) = @_;
-    my $validated_value = $v->param($key);
-    $c->param( $key => $validated_value ) if $validated_value;
+sub _replace_key {
+    my ( $self, $arg ) = @_;
+    $arg =~ s/\.\d/.[]/g;
+    return $arg;
 }
-
-sub _replace_key { my $arg = shift; $arg =~ s/\.\d/.[]/g; $arg; }
 
 1;
 __END__
@@ -348,25 +232,6 @@ Passing a scalar reffernce as an arguments to the validator method expands from 
 
 =head1 ATTRIBUTES
 
-=head2 C<controller>
-
-    my $controller = $fieldset->controller;
-    $fieldset->controller( Mojolicious::Controller->new );
-
-Return L<Mojolicious::Controller> object.
-
-=head2 C<is_validated>
-
-    my $bool = $fieldset->is_validated;
-
-Return boolean value.
-
-=head2 C<validation>
-
-    $validation = $fieldset->validation;
-
-$controller->validation alias.
-
 =head1 FUNCTIONS
 
 L<Yetie::Form::FieldSet> inherits all functions from L<Mojo::Base> and implements
@@ -448,59 +313,9 @@ Object once created are cached in "$fieldset->{_field}->{$field_key}".
     # { field_key => [ 'filter1', 'filter2', ... ], field_key2 => [ 'filter1', 'filter2', ... ] }
     my $filters = $fieldset->filters;
 
-=head2 C<has_data>
-
-    $bool = $fieldset->has_data;
-
-L<Mojolicious::Validator::Validation/has_data>
-
-=head2 C<param>
-
-    # Return scalar
-    my $param = $fieldset->param('name');
-
-    # Return array refference
-    my $param = $fieldset->param('favorite[]')
-
-The parameter is a validated values.
-This method should be called after the "validate" method.
-
-=head2 C<params>
-
-    my $validated_params = $fieldset->params;
-
-Return L<Mojo::Parameters> object.
-All parameters are validated values.
-This method should be called after the "validate" method.
-
 =head2 C<remove>
 
     $fieldset->remove('field_name');
-
-=head2 C<render_error>
-
-    $fieldset->render_error('email');
-
-If `$c->validation` has an error message it rendering HTML error message block.
-
-=head2 C<render_help>
-
-    $fieldset->render_help('email');
-
-Rendering HTML help block.
-
-=head2 C<render_label>
-
-    $fieldset->render_label('email');
-
-Rendering HTML label tag.
-
-=head2 C<render>
-
-    $fieldset->render('email');
-    $fieldset->render('email', value => 'foo', placeholder => 'bar' );
-
-Rendering HTML form widget(field or fields).
 
 =head2 C<schema>
 
@@ -509,23 +324,6 @@ Rendering HTML form widget(field or fields).
     my $field_schema = $fieldset->schema('field_key');
 
 Return hash refference. Get a field definition.
-
-=head2 C<scope_param>
-
-    my $scope = $fieldset->scope_param('user');
-
-Return hash refference or array refference.
-The parameter is a validated values.
-This method should be called after the "validate" method.
-
-Get expanded parameter. SEE L<CGI::Expand/expand_hash>
-
-=head2 C<validate>
-
-    my $bool = $fieldset->validate;
-    say 'Validation failure!' unless $bool;
-
-Return boolean. success return true.
 
 =head1 SEE ALSO
 
