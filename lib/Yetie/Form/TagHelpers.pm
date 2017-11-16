afe6d203ace31f1bb47fa930426d03ae5aad177e
clicktx <clicktx@gmail.com>
2017-11-16 09:16:44 +0900

    add Yetie::Form::TagHelpers

---
+++ lib/Yetie/Form/TagHelpers.pm
@@ -0,0 +1,28 @@
+package Yetie::Form::TagHelpers;
+use Mojo::Base -base;
+
+1;
+__END__
+=encoding utf8
+
+=head1 NAME
+
+Yetie::Form::TagHelpers
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Form::TagHelpers> inherits all attributes from L<Mojo::Base> and implements the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Form::TagHelpers> inherits all methods from L<Mojo::Base> and implements the following new ones.
+
+=head1 SEE ALSO
+
+L<Yetie::Form>, L<Yetie::Form::FieldSet>, L<Yetie::Form::Field>
+
+=cut
