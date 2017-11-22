2e999d297f0376068d55ae81bd503669c2c77195
clicktx <clicktx@gmail.com>
2017-11-23 06:26:12 +0900

    fix page category

--- lib/Yetie/Controller/Catalog/Category.pm
+++ lib/Yetie/Controller/Catalog/Category.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Yetie::Controller::Catalog';
 sub index {
     my $self = shift;
 
-    my $form = $self->form_set('search');
+    my $form = $self->form('search');
     $self->init_form();
 
     # return $self->render() unless $form->has_data;
