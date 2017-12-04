438b0167e02097150fc00cda49ef5ae44e04c661
clicktx <clicktx@gmail.com>
2017-12-04 21:39:26 +0900

    fix required parent_id

--- lib/Yetie/Form/FieldSet/Admin/Category.pm
+++ lib/Yetie/Form/FieldSet/Admin/Category.pm
@@ -3,10 +3,10 @@ use Mojo::Base -strict;
 use Yetie::Form::FieldSet;
 
 has_field 'title' => (
-    type        => 'text',
-    label       => 'Category Name',
-    required    => 1,
-    validations => [],
+    type           => 'text',
+    label          => 'Category Name',
+    required       => 1,
+    validations    => [],
     error_messages => {
         duplicate_title => 'A category with the same name exists.',
     },
@@ -14,7 +14,8 @@ has_field 'title' => (
 
 has_field 'parent_id' => (
     type     => 'choice',
-    label => 'Parent Category',
+    label    => 'Parent Category',
+    required => 1,
     expanded => 0,
     multiple => 0,
     choices  => [],
