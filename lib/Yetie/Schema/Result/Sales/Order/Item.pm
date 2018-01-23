27d9be559f00af29380804a45efadeadca7a4404
clicktx <clicktx@gmail.com>
2018-01-23 18:17:39 +0900

    fix relation attribute

--- lib/Yetie/Schema/Result/Sales/Order/Item.pm
+++ lib/Yetie/Schema/Result/Sales/Order/Item.pm
@@ -35,7 +35,7 @@ column price => Yetie::Schema::Result::Product->column_info('price');
 
 # NOTE: 'order' is SQL reserved word.
 belongs_to
-  an_order => 'Yetie::Schema::Result::Sales::Order',
+  sales_order => 'Yetie::Schema::Result::Sales::Order',
   { 'foreign.id' => 'self.order_id' };
 
 belongs_to
