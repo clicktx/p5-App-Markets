91cd0d34e136708cf0be3911448bdd1fa326dd51
clicktx <clicktx@gmail.com>
2018-02-13 05:47:15 +0900

    move page_title value

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -8,18 +8,22 @@ sub billing_address {
     my $order_id = $self->stash('id');
 
     my $order = $self->service('order')->find_order($order_id);
+    $order->page_title( $order->page_title . '/' . 'Edit Billing Address' );
+
     $self->stash( content => $order );
     return $self->reply->not_found if $order->is_empty;
 
-    my $form = $self->form('billing_address');
-
+    # Set default values
     # $self->form_default_value( $form, $entity );
+    my $form = $self->form('billing_address');
     $form->field($_)->default_value( $order->billing_address->$_ ) for qw(line1);
 
     return $self->render() unless $form->has_data;
-    $form->do_validate;
+    return $self->render() unless $form->do_validate;
 
-    return $self->render();
+    # update data
+
+    return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
 }
 
 1;
