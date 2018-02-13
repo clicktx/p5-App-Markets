6495afece6e66dba0073e3845c453030de07bbea
clicktx <clicktx@gmail.com>
2018-02-14 06:36:49 +0900

    revert help message

--- lib/Yetie/Form/FieldSet/Address.pm
+++ lib/Yetie/Form/FieldSet/Address.pm
@@ -7,7 +7,7 @@ has_field 'line1' => (
     autocomplete => 'address-line1',
     type         => 'text',
     placeholder  => '2125 Chestnut st',
-    help         => 'Street address, P.O. box, c/o',
+    help         => 'Street address, P.O. box, company name, c/o',
     filters      => [qw(trim)],
     validations  => [],
 );
