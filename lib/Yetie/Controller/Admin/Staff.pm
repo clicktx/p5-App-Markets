6a4a48ada711032be3a5b5732345c9b92529a833
clicktx <clicktx@gmail.com>
2018-02-22 19:23:26 +0900

    implement find_staff() at Service

--- lib/Yetie/Controller/Admin/Staff.pm
+++ lib/Yetie/Controller/Admin/Staff.pm
@@ -26,7 +26,7 @@ sub login {
 
     my $login_id = $form->param('login_id');
     my $password = $form->param('password');
-    my $staff    = $self->factory('staff')->build($login_id);
+    my $staff    = $self->service('staff')->find_staff($login_id);
 
     if ( $staff->id ) {
         if ( $self->scrypt_verify( $password, $staff->password->hash ) ) {
