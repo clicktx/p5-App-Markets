b5f6c5e8c5a7b63953c6e8493346367d6022a69b
clicktx <clicktx@gmail.com>
2018-03-01 12:03:33 +0900

    implement method "is_staff" and attribute "logged_in"

--- lib/Yetie/Domain/Entity/Staff.pm
+++ lib/Yetie/Domain/Entity/Staff.pm
@@ -2,9 +2,12 @@ package Yetie::Domain::Entity::Staff;
 use Yetie::Domain::Entity;
 
 has login_id   => undef;
+has logged_in  => 0;
 has created_at => undef;
 has updated_at => undef;
 has password   => sub { __PACKAGE__->factory('entity-password') };
+
+sub is_staff { shift->id ? 1 : 0 }
 
 1;
 __END__
@@ -26,6 +29,24 @@ the following new ones.
 
     my $id = $staff->id;
 
+=head2 C<logged_in>
+
+    my $bool = $staff->logged_in;
+
+Returns C<true> if the staff is logged in.
+
+=head2 C<login_id>
+
+    my $login_id = $staff->login_id;
+
+Return LoginID.
+
+=head2 C<password>
+
+    my $password = $customer->password;
+
+Return L<Yetie::Domain::Entity::Password> object.
+
 =head2 C<created_at>
 
 =head2 C<updated_at>
@@ -35,10 +56,16 @@ the following new ones.
 L<Yetie::Domain::Entity::Staff> inherits all methods from L<Yetie::Domain::Entity> and implements
 the following new ones.
 
+=head2 C<is_staff>
+
+    my $bool = $staff->is_staff;
+
+Return boolean value.
+
 =head1 AUTHOR
 
 Yetie authors.
 
 =head1 SEE ALSO
 
-L<Yetie::Domain::Entity::Account>, L<Yetie::Domain::Entity>
+L<Yetie::Domain::Entity::Password>, L<Yetie::Domain::Entity>
---
+++ t/domain/entity/staff.t
@@ -0,0 +1,27 @@
+use Mojo::Base -strict;
+use Test::More;
+
+use_ok 'Yetie::Domain::Entity::Staff';
+my $pkg = 'Yetie::Domain::Entity::Staff';
+
+subtest 'basic' => sub {
+    my $staff = $pkg->new( id => 1 );
+    isa_ok $staff, 'Yetie::Domain::Entity';
+
+    is $staff->id, 1, 'right staff ID';
+    isa_ok $staff->password, 'Yetie::Domain::Entity::Password';
+    is $staff->logged_in, 0, 'right not logged_in';
+
+    can_ok $staff, 'login_id';
+    can_ok $staff, 'created_at';
+    can_ok $staff, 'updated_at';
+};
+
+subtest 'is_staff' => sub {
+    my $staff = $pkg->new();
+    ok !$staff->is_staff;
+    $staff = $pkg->new( id => 1 );
+    ok $staff->is_staff;
+};
+
+done_testing();
