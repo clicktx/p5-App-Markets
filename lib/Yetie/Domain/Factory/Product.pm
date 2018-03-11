99f289966a8fcdbb3ca407cb27cad782eecb69b1
clicktx <clicktx@gmail.com>
2018-03-11 11:01:47 +0900

    improve Entity::Product inherit Entity::Page

--- lib/Yetie/Domain/Factory/Product.pm
+++ lib/Yetie/Domain/Factory/Product.pm
@@ -1,43 +1,6 @@
 package Yetie::Domain::Factory::Product;
 use Mojo::Base 'Yetie::Domain::Factory';
 
-sub build {
-    my ( $self, $product_id ) = @_;
-
-    my $schema  = $self->app->schema;
-    my $columns = [
-        qw(
-          id
-          title
-          description
-          price
-          created_at
-          updated_at
-          )
-    ];
-    my $itr = $schema->resultset('Product')->search( { id => $product_id }, { columns => $columns } );
-    my $data = $itr->hashref_first || {};
-
-    # Categories
-    my $product_categories = $schema->resultset('Product::Category')->get_product_categories_arrayref($product_id);
-    $data->{product_categories} = $product_categories;
-
-    # Breadcrumb
-    my $primary_category = $data->{product_categories}->[0];
-    my @breadcrumb;
-    my $result = $schema->resultset('Category')->find( $primary_category->{category_id} );
-    $result->ancestors->each(
-        sub {
-            my $category = shift;
-            push @breadcrumb, $self->_create_link( $category->id, $category->title );
-        }
-    ) if $result;
-    push @breadcrumb, $self->_create_link( $primary_category->{category_id}, $primary_category->{title} );
-    $data->{breadcrumb} = \@breadcrumb;
-
-    return $self->create($data);
-}
-
 sub cook {
     my $self = shift;
 
@@ -46,17 +9,8 @@ sub cook {
     $self->aggregate_collection( product_categories => 'entity-product-category', $product_categories || [] );
 
     # Aggregate breadcrumb
-    $self->aggregate_collection( breadcrumb => 'entity-link', $self->param('breadcrumb') || [] );
-}
-
-sub _create_link {
-    my ( $self, $category_id, $title ) = @_;
-    return {
-        title => $title,
-        url   => $self->app->url_for(
-            'RN_category' => { category_name => $title, category_id => $category_id }
-        )->to_string
-    };
+    my $breadcrumbs = $self->param('breadcrumbs');
+    $self->aggregate_collection( breadcrumbs => 'entity-breadcrumb', $breadcrumbs || [] );
 }
 
 1;
@@ -84,14 +38,6 @@ the following new ones.
 
 L<Yetie::Domain::Factory::Product> inherits all methods from L<Yetie::Domain::Factory> and implements
 the following new ones.
-
-=head2 C<build>
-
-    my $entity = $factory->build( $product_id );
-
-Return L<Yetie::Domain::Entity::Product> object.
-
-Create entity by product ID.
 
 =head1 AUTHOR
 
