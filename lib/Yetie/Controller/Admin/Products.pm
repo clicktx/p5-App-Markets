47f62ad909bff97520d787a4c2709f2f16e4d32f
clicktx <clicktx@gmail.com>
2018-03-15 16:51:55 +0900

    implement Service::Products

--- lib/Yetie/Controller/Admin/Products.pm
+++ lib/Yetie/Controller/Admin/Products.pm
@@ -1,13 +1,6 @@
 package Yetie::Controller::Admin::Products;
 use Mojo::Base 'Yetie::Controller::Admin';
 
-# sub init_form {
-#     my ( $self, $form, $rs ) = @_;
-#
-#     $form->field('product_id')->value('111');
-#     return $self->SUPER::init_form();
-# }
-
 sub index {
     my $self = shift;
 
@@ -17,33 +10,8 @@ sub index {
     # return $self->render() unless $form->has_data;
     $form->do_validate;
 
-    my $page     = $form->param('page')     || 1;
-    my $per_page = $form->param('per_page') || 5;
-
-    # 1page当たりの表示件数
-    # cookieに保存する
-    #limit -dmm
-    #items_per_page cs
-    #page_count
-
-    my $rs          = $self->app->schema->resultset('Product');
-    my $products_rs = $rs->search(
-        {},
-        {
-            order_by => { -desc => [ 'updated_at', 'created_at' ] },
-            page     => $page,
-            rows     => $per_page,
-        }
-    );
-
-    my $data = {
-        meta_title   => '',
-        breadcrumbs  => [],
-        form         => $form,
-        product_list => $products_rs->to_data( { no_relation => 1 } ),
-        pager        => $products_rs->pager,
-    };
-    my $products = $self->factory('entity-products')->create($data);
+    # use service
+    my $products = $self->service('products')->search_products($form);
 
     $self->stash( content => $products );
     $self->render();
