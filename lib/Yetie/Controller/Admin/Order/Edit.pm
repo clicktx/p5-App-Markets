07a49e64d85dcc20f3c9fef03e1a98afc6fdce1f
clicktx <clicktx@gmail.com>
2018-04-25 20:22:50 +0900

    implement _init_form method

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -4,33 +4,59 @@ use Mojo::Base 'Yetie::Controller::Admin';
 # NOTE: Catalog::Checkoutに関連する実装がある。
 # また、管理者注文も考慮する必要がある。
 sub billing_address {
-    my $self     = shift;
+    my $self = shift;
+
     my $order_id = $self->stash('id');
-
-    my $order = $self->service('order')->find_order($order_id);
+    my $order    = $self->service('order')->find_order($order_id);
     $order->page_title( $order->page_title . '/' . 'Edit Billing Address' );
-
     $self->stash( entity => $order );
     return $self->reply->not_found if $order->is_empty;
 
-    # Set default values
-    my $region = 'us';
-    my $fields = $order->billing_address->fields($region);
-    my $form   = $self->form('billing_address');
-    my @billing_address_fields;
-    do {
-        $form->field( "billing_address." . $_ )->default_value( $order->billing_address->$_ );
-        push @billing_address_fields, 'billing_address.' . $_;
-      }
-      for @{$fields};
-    $self->stash( fields => \@billing_address_fields );
-
+    my $form = $self->_init_form('billing_address');
     return $self->render() unless $form->has_data;
     return $self->render() unless $form->do_validate;
 
-    # update data
+    # update address
+    # ...
 
     return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
+}
+
+sub shipping_address {
+    my $self = shift;
+
+    my $order_id = $self->stash('id');
+    my $order    = $self->service('order')->find_order($order_id);
+    $order->page_title( $order->page_title . '/' . 'Edit Shipping Address' );
+    $self->stash( entity => $order );
+    return $self->reply->not_found if $order->is_empty;
+
+    my $form = $self->_init_form('shipping_address');
+    return $self->render() unless $form->has_data;
+    return $self->render() unless $form->do_validate;
+
+    # update address
+    # ...
+
+    return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
+}
+
+sub _init_form {
+    my ( $self, $address_type ) = @_;
+
+    my $region = 'us';
+    my $form   = $self->form($address_type);
+    my $order  = $self->stash('entity');
+
+    # Set form default value
+    my $fields = $order->$address_type->fields($region);
+    $form->field("$address_type.$_")->default_value( $order->$address_type->$_ ) for @{$fields};
+
+    # Collate field keys
+    my @field_keys = map { "$address_type.$_" } @{$fields};
+    $self->stash( fields => \@field_keys );
+
+    return $form;
 }
 
 1;
