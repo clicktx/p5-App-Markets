91021d644568fdbee8cc9f61a7b578d48adcd7e3
clicktx <clicktx@gmail.com>
2018-05-09 06:32:47 +0900

    fix form default parameter

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -4,8 +4,6 @@ use Mojo::Base 'Yetie::Controller::Admin';
 # NOTE: Catalog::Checkoutに関連する実装がある。
 # また、管理者注文も考慮する必要がある。
 sub billing_address {
-    my $self = shift;
-
     my $order_id = $self->stash('id');
     my $order    = $self->service('order')->find_order($order_id);
     $order->page_title( $order->page_title . '/' . 'Edit Billing Address' );
@@ -13,11 +11,12 @@ sub billing_address {
     return $self->reply->not_found if $order->is_empty;
 
     my $form = $self->_init_form('billing_address');
-    return $self->render() unless $form->has_data;
-    return $self->render() unless $form->do_validate;
+
+    # GET Request
+    return $self->render() if $self->req->method eq 'GET';
 
     # update address
-    # ...
+    return $self->render() unless $form->do_validate;
 
     return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
 }
@@ -50,7 +49,8 @@ sub _init_form {
 
     # Set form default value
     my $fields = $order->$address_type->fields($region);
-    $form->field("$address_type.$_")->default_value( $order->$address_type->$_ ) for @{$fields};
+    my $params = $self->req->params;
+    $params->append( "$address_type.$_" => $order->$address_type->$_ ) for @{$fields};
 
     # Collate field keys
     my @field_keys = map { "$address_type.$_" } @{$fields};
