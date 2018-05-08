26711b0290f6cba853bd6549f9d1d725d038d724
clicktx <clicktx@gmail.com>
2018-05-09 06:43:08 +0900

    integrete edit address method billing and shipping

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -4,38 +4,42 @@ use Mojo::Base 'Yetie::Controller::Admin';
 # NOTE: Catalog::Checkoutに関連する実装がある。
 # また、管理者注文も考慮する必要がある。
 sub billing_address {
+    my $self = shift;
+    $self->_edit_address(
+        {
+            address_type => 'billing_address',
+            title        => 'Edit Billing Address',
+        }
+    );
+}
+
+sub shipping_address {
+    my $self = shift;
+    $self->_edit_address(
+        {
+            address_type => 'shipping_address',
+            title        => 'Edit Shipping Address',
+        }
+    );
+}
+
+sub _edit_address {
+    my $self = shift;
+    my $args = shift;
+
     my $order_id = $self->stash('id');
     my $order    = $self->service('order')->find_order($order_id);
-    $order->page_title( $order->page_title . '/' . 'Edit Billing Address' );
+    $order->page_title( $order->page_title . '/' . $args->{title} );
     $self->stash( entity => $order );
     return $self->reply->not_found if $order->is_empty;
 
-    my $form = $self->_init_form('billing_address');
+    my $form = $self->_init_form( $args->{address_type} );
 
     # GET Request
     return $self->render() if $self->req->method eq 'GET';
 
     # update address
     return $self->render() unless $form->do_validate;
-
-    return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
-}
-
-sub shipping_address {
-    my $self = shift;
-
-    my $order_id = $self->stash('id');
-    my $order    = $self->service('order')->find_order($order_id);
-    $order->page_title( $order->page_title . '/' . 'Edit Shipping Address' );
-    $self->stash( entity => $order );
-    return $self->reply->not_found if $order->is_empty;
-
-    my $form = $self->_init_form('shipping_address');
-    return $self->render() unless $form->has_data;
-    return $self->render() unless $form->do_validate;
-
-    # update address
-    # ...
 
     return $self->redirect_to( 'RN_admin_order_details', order_id => $order_id );
 }
