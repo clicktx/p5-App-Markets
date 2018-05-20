f9fb78e5970736902185a765bab5a19ad092f1a2
clicktx <clicktx@gmail.com>
2018-05-21 06:27:02 +0900

    improve use base class

--- lib/Yetie/Form/FieldSet/Admin/Product/Category.pm
+++ lib/Yetie/Form/FieldSet/Admin/Product/Category.pm
@@ -1,12 +1,9 @@
 package Yetie::Form::FieldSet::Admin::Product::Category;
 use Mojo::Base -strict;
-use Yetie::Form::FieldSet;
+use Yetie::Form::FieldSet::Base::Product;
 
-has_field 'categories' => (
-    type        => 'choice',
-    expanded    => 1,
-    multiple    => 1,
-    validations => ['uint'],
-);
+my $base_class = 'Yetie::Form::FieldSet::Base::Product';
+
+has_field 'categories' => $base_class->field_info('categories');
 
 1;
--- lib/Yetie/Form/FieldSet/Product.pm
+++ lib/Yetie/Form/FieldSet/Product.pm
@@ -1,26 +1,11 @@
 package Yetie::Form::FieldSet::Product;
 use Mojo::Base -strict;
-use Yetie::Form::FieldSet;
+use Yetie::Form::FieldSet::Base::Product;
 
-has_field 'product_id' => (
-    type        => 'hidden',
-    filters     => [],
-    validations => [qw(uint)],
-);
+my $base_class = 'Yetie::Form::FieldSet::Base::Product';
 
-has_field 'quantity' => (
-    type          => 'text',
-    label         => 'Quantity',
-    default_value => 1,
-    filters       => [qw(trim)],
-    validations   => [ 'uint', [ min => 1 ] ],
-);
+has_field 'product_id' => $base_class->field_info('product_id');
 
-has_field 'categories' => (
-    type        => 'choice',
-    expanded    => 1,
-    multiple    => 1,
-    validations => ['uint'],
-);
+has_field 'quantity' => $base_class->field_info('quantity');
 
 1;
