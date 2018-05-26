226aa17cfd3fd9993d09b894cef425494a6a96db
clicktx <clicktx@gmail.com>
2018-05-26 19:23:22 +0900

    add preference "staff_password_min" and "staff_password_max"

--- share/default_data.pl
+++ share/default_data.pl
@@ -16,8 +16,12 @@
         [ 'locale_country', undef, 'US', 'pref.title.locale_country', 'pref.summary.locale_country', 100, 2 ],
         [ 'shop_name', undef, 'Yetie Shop', 'pref.title.shop_name', 'pref.summary.shop_name', 100, 2 ],
 
+        # staff
+        [ 'staff_password_min', undef, '4', 'pref.title.staff_password_min', 'pref.summary.staff_password_min', 100, 2 ],
+        [ 'staff_password_max', undef, '8', 'pref.title.staff_password_max', 'pref.summary.staff_password_max', 101, 2 ],
+
         # customer
-        [ 'customer_password_min', undef, '1', 'pref.title.customer_password_min', 'pref.summary.customer_password_min', 100, 2 ],
+        [ 'customer_password_min', undef, '4', 'pref.title.customer_password_min', 'pref.summary.customer_password_min', 100, 2 ],
         [ 'customer_password_max', undef, '8', 'pref.title.customer_password_max', 'pref.summary.customer_password_max', 101, 2 ],
     ],
 
