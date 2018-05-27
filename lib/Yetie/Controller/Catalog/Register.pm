c3a8e3e74cf5eb2f5843560bf4e6f21bc8aecca0
clicktx <clicktx@gmail.com>
2018-05-27 18:53:05 +0900

    implement mock for customer registration

--- lib/Yetie/Controller/Catalog/Register.pm
+++ lib/Yetie/Controller/Catalog/Register.pm
@@ -3,7 +3,34 @@ use Mojo::Base 'Yetie::Controller::Catalog';
 
 sub index {
     my $self = shift;
-    $self->render();
+
+    my $form = $self->form('catalog-register');
+    return $self->render() unless $form->has_data;
+
+    # Validation form
+    return $self->render() unless $form->do_validate;
+
+    # Registration customer data
+    # Create login session
+    $self->redirect_to('RN_customer_register_done');
+
+    #Registration – Activate a New Account by Email
+    # Verification Token
+    # The VerificationToken Entity
+    # Add the enabled Field to User
+    # Using a Spring Event to Create the Token and Send the Verification Email
+    #
+    # The Event and The Listener
+    # The RegistrationListener Handles the OnRegistrationCompleteEvent
+    # Processing the Verification Token Parameter
+    # RegistrationController Processing the Registration Confirmation
+    # Adding Account Activation Checking to the Login Process
+    # CustomAuthenticationFailureHandler:
+}
+
+sub done {
+    my $self = shift;
+    return $self->render();
 }
 
 1;
