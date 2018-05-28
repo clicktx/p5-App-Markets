bada4e617ebeda432aaa2304f37c072e235c71b1
clicktx <clicktx@gmail.com>
2018-05-29 06:41:24 +0900

    add admin/customers

---
+++ lib/Yetie/Controller/Admin/Customers.pm
@@ -0,0 +1,18 @@
+package Yetie::Controller::Admin::Customers;
+use Mojo::Base 'Yetie::Controller::Admin';
+
+sub index {
+    my $self = shift;
+
+    my $form = $self->form('search');
+    $self->init_form();# hook point
+    return $self->render() unless $form->do_validate;
+
+    # my $orders = $self->service('orders')->search_orders($form);
+    my $customers = $self->factory('customers')->create( { form => $form } );
+    $self->stash( entity => $customers );
+
+    $self->render();
+}
+
+1;
