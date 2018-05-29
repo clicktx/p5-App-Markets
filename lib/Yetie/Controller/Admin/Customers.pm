564d2ce604a7e0ed26ffe4f2f430539ba1ac69fe
clicktx <clicktx@gmail.com>
2018-05-29 18:19:47 +0900

    add page_title

--- lib/Yetie/Controller/Admin/Customers.pm
+++ lib/Yetie/Controller/Admin/Customers.pm
@@ -5,12 +5,15 @@ sub index {
     my $self = shift;
 
     my $form = $self->form('search');
-    $self->init_form();# hook point
+    $self->init_form();    # hook point
     return $self->render() unless $form->do_validate;
 
     # my $orders = $self->service('orders')->search_orders($form);
     my $customers = $self->factory('customers')->create( { form => $form } );
     $self->stash( entity => $customers );
+
+    # Page data
+    $customers->page_title('Customers');
 
     $self->render();
 }
