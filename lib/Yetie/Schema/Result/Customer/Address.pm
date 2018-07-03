1f33fe9e5669098f3dd0c421bf66741156978c6e
clicktx <clicktx@gmail.com>
2018-07-04 06:25:13 +0900

    fix schema to composite primary keys

--- lib/Yetie/Schema/Result/Customer/Address.pm
+++ lib/Yetie/Schema/Result/Customer/Address.pm
@@ -2,27 +2,20 @@ package Yetie::Schema::Result::Customer::Address;
 use Mojo::Base 'Yetie::Schema::Base::Result';
 use DBIx::Class::Candy -autotable => v1;
 
-primary_column id => {
-    data_type         => 'INT',
-    is_auto_increment => 1,
-};
-
-column customer_id => {
+primary_column customer_id => {
     data_type   => 'INT',
     is_nullable => 0,
 };
 
-column address_id => {
+primary_column address_id => {
     data_type   => 'INT',
     is_nullable => 0,
 };
 
-column address_type_id => {
+primary_column address_type_id => {
     data_type   => 'INT',
     is_nullable => 0,
 };
-
-unique_constraint ui_customer_id_address_id_address_type_id => [qw/customer_id address_id address_type_id/];
 
 belongs_to
   customer => 'Yetie::Schema::Result::Customer',
