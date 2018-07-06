9ad6de87997d08e329699c2353509f213d258cf9
clicktx <clicktx@gmail.com>
2018-07-06 17:42:34 +0900

    add method number_only

--- lib/Yetie/Domain/Entity/Address/Phone.pm
+++ lib/Yetie/Domain/Entity/Address/Phone.pm
@@ -1,9 +1,14 @@
 package Yetie::Domain::Entity::Address::Phone;
 use Yetie::Domain::Entity;
 
-has type        => '';
-has number      => '';
-has number_only => '';
+has number => '';
+has type   => '';
+
+sub number_only {
+    local $_ = shift->number;
+    s/\D//g;
+    $_;
+}
 
 1;
 __END__
@@ -21,10 +26,18 @@ Yetie::Domain::Entity::Address::Phone
 L<Yetie::Domain::Entity::Address::Phone> inherits all attributes from L<Yetie::Domain::Entity> and implements
 the following new ones.
 
+=head2 C<number>
+
+=head2 C<type>
+
 =head1 METHODS
 
 L<Yetie::Domain::Entity::Address::Phone> inherits all methods from L<Yetie::Domain::Entity> and implements
 the following new ones.
+
+=head2 C<number_only>
+
+    my $number_only = $phone->number_only;
 
 =head1 AUTHOR
 
