ab679cc00a687f98ec0d0fa036335e11cfc99009
clicktx <clicktx@gmail.com>
2018-08-19 11:34:02 +0900

    implement method "is_verify"

--- lib/Yetie/Domain/Value/Password.pm
+++ lib/Yetie/Domain/Value/Password.pm
@@ -1,8 +1,14 @@
 package Yetie::Domain::Value::Password;
 use Yetie::Domain::Value;
+use Crypt::ScryptKDF qw(scrypt_hash_verify);
 
 has created_at => undef;
 has updated_at => undef;
+
+sub is_verify {
+    my ( $self, $raw_password ) = @_;
+    return scrypt_hash_verify( $raw_password, $self->value ) ? 1 : 0;
+}
 
 1;
 __END__
@@ -24,6 +30,12 @@ the following new ones.
 
 L<Yetie::Domain::Value::Password> inherits all methods from L<Yetie::Domain::Value> and implements
 the following new ones.
+
+=head2 C<is_verify>
+
+    my $bool = $password->is_verify($raw_password);
+
+Return boolean value.
 
 =head1 AUTHOR
 
