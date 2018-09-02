22d56e0f759a7354bca3c8d2d86027d645f73ce1
clicktx <clicktx@gmail.com>
2018-09-03 07:18:01 +0900

    add entity addresses

---
+++ lib/Yetie/Domain/Entity/Addresses.pm
@@ -0,0 +1,35 @@
+package Yetie::Domain::Entity::Addresses;
+use Yetie::Domain::List;
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::Entity::Addresses
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Entity::Addresses> inherits all attributes from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head2 C<item_list>
+
+Return C<Yetie::Domain::Collection> object.
+
+=head1 METHODS
+
+L<Yetie::Domain::Entity::Addresses> inherits all methods from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Domain::List>
---
+++ lib/Yetie/Domain/Factory/Entity/Addresses.pm
@@ -0,0 +1,43 @@
+package Yetie::Domain::Factory::Entity::Addresses;
+use Mojo::Base 'Yetie::Domain::Factory';
+
+sub cook {
+    my $self = shift;
+
+    $self->aggregate_collection( list => 'entity-address', $self->param('list') || [] );
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::Factory::Entity::Addresses
+
+=head1 SYNOPSIS
+
+    my $entity = Yetie::Domain::Factory::Entity::Addresses->new( %args )->create;
+
+    # In controller
+    my $entity = $c->factory('entity-addresses')->create(%args);
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Factory::Entity::Addresses> inherits all attributes from L<Yetie::Domain::Factory> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Domain::Factory::Entity::Addresses> inherits all methods from L<Yetie::Domain::Factory> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+ L<Yetie::Domain::Factory>
