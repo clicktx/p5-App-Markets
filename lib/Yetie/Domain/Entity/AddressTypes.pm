897719019e65565c5c40006f78d2395728266c7d
clicktx <clicktx@gmail.com>
2018-09-07 18:54:48 +0900

    add Entity::AddressTypes

---
+++ lib/Yetie/Domain/Entity/AddressTypes.pm
@@ -0,0 +1,31 @@
+package Yetie::Domain::Entity::AddressTypes;
+use Yetie::Domain::Base 'Yetie::Domain::List';
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::Entity::AddressTypes
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Entity::AddressTypes> inherits all attributes from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Domain::Entity::AddressTypes> inherits all methods from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Domain::List>
