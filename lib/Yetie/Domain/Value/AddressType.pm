f2158b06cfd2d8b5288275d801f40491619dc2b3
clicktx <clicktx@gmail.com>
2018-09-07 18:57:13 +0900

    add Value::AddressType

---
+++ lib/Yetie/Domain/Value/AddressType.pm
@@ -0,0 +1,40 @@
+package Yetie::Domain::Value::AddressType;
+use Yetie::Domain::Value;
+
+has [qw(id name label)];
+has value => sub { shift->id };
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::Value::AddressType
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Value::AddressType> inherits all attributes from L<Yetie::Domain::Value> and implements
+the following new ones.
+
+=head2 C<id>
+
+=head2 C<name>
+
+=head2 C<label>
+
+=head1 METHODS
+
+L<Yetie::Domain::Value::AddressType> inherits all methods from L<Yetie::Domain::Value> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Domain::Value>
