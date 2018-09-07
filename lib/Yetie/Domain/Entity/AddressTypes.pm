d3923b8fdff63abf4888cddf720c43b9dc087002
clicktx <clicktx@gmail.com>
2018-09-07 19:12:27 +0900

    implement method get_id_by_name

--- lib/Yetie/Domain/Entity/AddressTypes.pm
+++ lib/Yetie/Domain/Entity/AddressTypes.pm
@@ -1,6 +1,12 @@
 package Yetie::Domain::Entity::AddressTypes;
 use Yetie::Domain::Base 'Yetie::Domain::List';
 
+sub get_id_by_name {
+    my ( $self, $name ) = @_;
+    my $address_type = $self->list->first( sub { $_->name eq $name } );
+    $address_type ? $address_type->id : undef;
+}
+
 1;
 __END__
 
@@ -21,6 +27,10 @@ the following new ones.
 
 L<Yetie::Domain::Entity::AddressTypes> inherits all methods from L<Yetie::Domain::List> and implements
 the following new ones.
+
+=head2 C<get_id_by_name>
+
+    my $address_type_id = $entity->get_id_by_name('foo');
 
 =head1 AUTHOR
 
