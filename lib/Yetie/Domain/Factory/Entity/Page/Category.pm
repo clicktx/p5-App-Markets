d42997d5570450660fe6ef393bfe0ba7519333cc
clicktx <clicktx@gmail.com>
2018-09-10 21:05:46 +0900

    fix rename Domain::Page::Category

--- lib/Yetie/Domain/Factory/Entity/Page/Category.pm
+++ lib/Yetie/Domain/Factory/Entity/Page/Category.pm
@@ -1,4 +1,4 @@
-package Yetie::Domain::Factory::Entity::Category;
+package Yetie::Domain::Factory::Entity::Page::Category;
 use Mojo::Base 'Yetie::Domain::Factory';
 
 sub cook {
@@ -18,25 +18,25 @@ __END__
 
 =head1 NAME
 
-Yetie::Domain::Factory::Entity::Category
+Yetie::Domain::Factory::Entity::Page::Category
 
 =head1 SYNOPSIS
 
-    my $entity = Yetie::Domain::Factory::Entity::Category->new( %args )->create;
+    my $entity = Yetie::Domain::Factory::Entity::Page::Category->new( %args )->create;
 
     # In controller
-    my $entity = $c->factory('entity-category')->create(%args);
+    my $entity = $c->factory('entity-page-category')->create(%args);
 
 =head1 DESCRIPTION
 
 =head1 ATTRIBUTES
 
-L<Yetie::Domain::Factory::Entity::Category> inherits all attributes from L<Yetie::Domain::Factory> and implements
+L<Yetie::Domain::Factory::Entity::Page::Category> inherits all attributes from L<Yetie::Domain::Factory> and implements
 the following new ones.
 
 =head1 METHODS
 
-L<Yetie::Domain::Factory::Entity::Category> inherits all methods from L<Yetie::Domain::Factory> and implements
+L<Yetie::Domain::Factory::Entity::Page::Category> inherits all methods from L<Yetie::Domain::Factory> and implements
 the following new ones.
 
 =head1 AUTHOR
