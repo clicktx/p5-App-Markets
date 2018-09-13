d2432f5e8b6f29ec82a712117b1439e9f573ff10
clicktx <clicktx@gmail.com>
2018-09-14 06:27:52 +0900

    add domain List::CartShipments

---
+++ lib/Yetie/Domain/List/CartShipments.pm
@@ -0,0 +1,31 @@
+package Yetie::Domain::List::CartShipments;
+use Yetie::Domain::Base 'Yetie::Domain::List';
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::List::CartShipments
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::List::CartShipments> inherits all attributes from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Domain::List::CartShipments> inherits all methods from L<Yetie::Domain::List> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Domain::Entity::Item>, L<Yetie::Domain::List>
