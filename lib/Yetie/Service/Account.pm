d8a46a07713ca830113b26fe80f6c30573eea33b
clicktx <clicktx@gmail.com>
2018-11-13 09:26:46 +0900

    implement Service::Account

---
+++ lib/Yetie/Service/Account.pm
@@ -0,0 +1,65 @@
+package Yetie::Service::Account;
+use Mojo::Base 'Yetie::Service';
+use Yetie::Util qw(uuid);
+
+sub create_token {
+    my ( $self, $email ) = @_;
+
+    # Token
+    my $token = uuid();
+
+    # Request IP
+    # NOTE: 'X-Real-IP', 'X-Forwarded-For'はどうする？
+    my $request_ip = $self->controller->tx->remote_address || 'none';
+
+    # Expires
+    my $expires = $self->factory('value-expires')->construct();
+
+    # Store to DB
+    $self->resultset('AuthorizationRequest')->create(
+        {
+            email      => $email,
+            token      => $token,
+            request_ip => $request_ip,
+            expires    => $expires,
+        }
+    );
+    return $token;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Service::Account
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Service::Account> inherits all attributes from L<Yetie::Service> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Service::Account> inherits all methods from L<Yetie::Service> and implements
+the following new ones.
+
+=head2 C<create_token>
+
+    my token = $servece->create_token($email);
+
+Create one-time token and store it in the DB.
+
+Return token string.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Service>
---
+++ t/service/account.t
@@ -0,0 +1,26 @@
+use Mojo::Base -strict;
+
+use t::Util;
+use Test::More;
+use Test::Mojo;
+
+my $t   = Test::Mojo->new('App');
+my $app = $t->app;
+
+sub _init {
+    my $controller = $app->build_controller;
+    my $service    = $controller->service('account');
+    return ( $controller, $service );
+}
+
+subtest 'create_token' => sub {
+    my ( $c, $s ) = _init();
+    my $rs      = $c->resultset('AuthorizationRequest');
+    my $last_id = $rs->last_id;
+
+    my $r = qr/[0-9A-F]/;
+    like $s->create_token('foo@example.org'), qr/$r{8}\-$r{4}\-4$r{3}\-[89AB]$r{3}\-$r{12}/, 'right token';
+    isnt $last_id, $rs->last_id, 'right store to DB';
+};
+
+done_testing();
