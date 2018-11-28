ee8d5ec1d7947a64890578aea96962752f959c31
clicktx <clicktx@gmail.com>
2018-11-28 19:20:33 +0900

    improve Domain::List::Customers

---
+++ lib/Yetie/Factory/List/Customers.pm
@@ -0,0 +1,42 @@
+package Yetie::Factory::List::Customers;
+use Mojo::Base 'Yetie::Factory';
+
+sub cook {
+    my $self = shift;
+
+    $self->aggregate_collection( list => 'entity-customer', $self->param('list') || [] );
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Factory::List::Customers
+
+=head1 SYNOPSIS
+
+    my $list = Yetie::Factory::List::Customers->new()->construct();
+
+    # In controller
+    my $list = $c->factory('list-customers')->construct();
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Factory::List::Customers> inherits all attributes from L<Yetie::Factory> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Factory::List::Customers> inherits all methods from L<Yetie::Factory> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+ L<Yetie::Factory>
