9a914cd2b8ed03c05b832f986815ca9356374496
clicktx <clicktx@gmail.com>
2018-11-28 19:26:55 +0900

    remove method "each"

--- lib/Yetie/Domain/Entity/Page/Orders.pm
+++ lib/Yetie/Domain/Entity/Page/Orders.pm
@@ -3,8 +3,6 @@ use Yetie::Domain::Base 'Yetie::Domain::Entity::Page';
 
 has order_list => sub { Yetie::Domain::Collection->new };
 
-sub each { shift->order_list->each(@_) }
-
 1;
 __END__
 
@@ -31,13 +29,6 @@ Return L<Yetie::Domain::Collection> object.
 
 L<Yetie::Domain::Entity::Page::Orders> inherits all methods from L<Yetie::Domain::Entity::Page> and implements
 the following new ones.
-
-=head2 C<each>
-
-    $orders->each(...);
-
-    # Longer version
-    $orders->order_list->each(...);
 
 =head1 AUTHOR
 
