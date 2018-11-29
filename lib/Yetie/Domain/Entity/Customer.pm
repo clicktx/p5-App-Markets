e89e9985f4184dd309b295422d4d7fdd213e2673
clicktx <clicktx@gmail.com>
2018-11-29 09:03:10 +0900

    fix method is_registerd and implement method has_password

--- lib/Yetie/Domain/Entity/Customer.pm
+++ lib/Yetie/Domain/Entity/Customer.pm
@@ -7,7 +7,9 @@ has updated_at => undef;
 has password   => sub { __PACKAGE__->factory('value-password')->construct() };
 has emails     => sub { __PACKAGE__->factory('list-emails')->construct() };
 
-sub is_registered { shift->password->value ? 1 : 0 }
+sub has_password { shift->password->value ? 1 : 0 }
+
+sub is_registered { shift->id ? 1 : 0 }
 
 1;
 __END__
@@ -53,6 +55,12 @@ Return L<Yetie::Domain::Value::Password> object.
 
 L<Yetie::Domain::Entity::Customer> inherits all methods from L<Yetie::Domain::Entity> and implements
 the following new ones.
+
+=head2 C<has_password>
+
+    my $bool = $customer->has_password;
+
+Returns true if a password has been set.
 
 =head2 C<is_registered>
 
