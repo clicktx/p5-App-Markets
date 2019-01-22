52d736049ddcd6f61ba70ce922e6f61947b747fa
clicktx <clicktx@gmail.com>
2019-01-22 20:52:48 +0900

    add field "remember_me"

--- lib/Yetie/Form/FieldSet/Account/Login.pm
+++ lib/Yetie/Form/FieldSet/Account/Login.pm
@@ -2,8 +2,14 @@ package Yetie::Form::FieldSet::Account::Login;
 use Mojo::Base -strict;
 use Yetie::Form::FieldSet;
 
-has_field 'email' => extends('base-email#email');
+has_field email => extends('base-email#email');
 
-has_field 'password' => extends('base-password#customer_password');
+has_field password => extends('base-password#customer_password');
+
+has_field remember_me => (
+    type  => 'checkbox',
+    label => 'Remember me',
+    value => 1,
+);
 
 1;
