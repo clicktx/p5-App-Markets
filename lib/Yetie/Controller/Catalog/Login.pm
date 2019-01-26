17fc2ef46901d4bde1685aedad7ce2140df3c0a5
clicktx <clicktx@gmail.com>
2019-01-26 19:46:03 +0900

    improve cookie expires use preference

--- lib/Yetie/Controller/Catalog/Login.pm
+++ lib/Yetie/Controller/Catalog/Login.pm
@@ -88,7 +88,7 @@ sub with_password {
     return $c->render() unless $form->do_validate;
     $c->cookie(
         default_remember_me => $form->param('remember_me') ? 1 : 0,
-        { path => '/', expires => time + 60 * 60 * 24 * 365 }
+        { path => '/', expires => time + $c->pref('cookie_expires_long') }
     );
 
     # Login failure
