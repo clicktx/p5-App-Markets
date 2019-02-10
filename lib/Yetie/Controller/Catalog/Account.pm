94bdb8170e6301a47f170580a0b97289a202eee1
clicktx <clicktx@gmail.com>
2019-02-10 11:31:38 +0900

    add comment

--- lib/Yetie/Controller/Catalog/Account.pm
+++ lib/Yetie/Controller/Catalog/Account.pm
@@ -28,9 +28,20 @@ sub remember_me_handler {
 sub logout {
     my $c = shift;
 
+  # logout時にカスタマーカートの削除処理を入れるか？
+  # e.g. ３日間経過していたら後で買うリストにカート商品を移動して、カートを空にする
+
+    # logoutは自分の意志で行い、ブラウザから完全に個人情報を削除したい意思がある
+    # カート内のアイテムも削除　又は　後で買うリストに移動?
+    # lohaco toLaterBuyFromCart
+
+ # Amazonはサインアウトでカート内のアイテムを削除しない（日数が経過した場合は不明）
+
     # Remove server session
     my $session = $c->server_session;
-    $session->cart_session->remove;
+
+# カートセッションを削除するとDBに残っている無効なセッションも削除されるので良いが...
+# $session->cart_session->remove;
     $session->remove_session;
 
     # Remove auto login cookie & token
