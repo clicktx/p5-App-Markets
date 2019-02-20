3d279d3631c90aa19735a53281d6e093c1d56f1d
clicktx <clicktx@gmail.com>
2019-02-21 06:54:00 +0900

    fix data type

--- lib/Yetie/Schema/Result/AuthorizationRequest.pm
+++ lib/Yetie/Schema/Result/AuthorizationRequest.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Yetie::Schema::Base::Result';
 use DBIx::Class::Candy -autotable => v1;
 
 primary_column id => {
-    data_type         => 'INT',
+    data_type         => 'BIGINT',
     is_auto_increment => 1,
 };
 
