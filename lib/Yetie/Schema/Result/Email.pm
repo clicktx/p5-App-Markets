eaed5f24f2dad396cf0afefaeaa5790a30d5a6c9
clicktx <clicktx@gmail.com>
2019-03-07 18:48:41 +0900

    improve column type and remove unique index

--- lib/Yetie/Schema/Result/Email.pm
+++ lib/Yetie/Schema/Result/Email.pm
@@ -8,7 +8,7 @@ primary_column id => {
 };
 
 # NOTE: Column that use index have a max key length is 767 bytes.
-column address => {
+unique_column address => {
     data_type   => 'VARCHAR',
     size        => 128,
     is_nullable => 0,
@@ -17,9 +17,7 @@ column address => {
 # Relation
 might_have
   customer_email => 'Yetie::Schema::Result::Customer::Email',
-  { 'foreign.email_id' => 'self.id' };
-
-# Index
-unique_constraint ui_address => [qw/address/];
+  { 'foreign.email_id' => 'self.id' },
+  { cascade_delete     => 0 };
 
 1;
