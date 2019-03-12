38853d469d657194fd13ed4f7c44b897057651dc
clicktx <clicktx@gmail.com>
2019-03-12 20:44:38 +0900

    remove unnecessary class

--- lib/Yetie/Schema/Result/Order/Sequence.pm
+++
@@ -1,9 +0,0 @@
-package Yetie::Schema::Result::Order::Sequence;
-use Mojo::Base 'Yetie::Schema::Result';
-use DBIx::Class::Candy -autotable => v1;
-
-primary_column id => {
-    data_type         => 'INT',
-};
-
-1;
