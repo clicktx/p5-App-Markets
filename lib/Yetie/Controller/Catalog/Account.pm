82849fc8859f61932fd4789f39ec2292a007b047
clicktx <clicktx@gmail.com>
2019-04-08 18:59:53 +0900

    remove function "authorize"

--- lib/Yetie/Controller/Catalog/Account.pm
+++ lib/Yetie/Controller/Catalog/Account.pm
@@ -1,20 +1,6 @@
 package Yetie::Controller::Catalog::Account;
 use Mojo::Base 'Yetie::Controller::Catalog';
 
-sub authorize {
-    my $c = shift;
-    return 1 if $c->is_logged_in;
-
-    # NOTE: 最終リクエストがPOSTの場合はhistoryから最後のGETリクエストを取得する？
-    #       sessionが切れている（はず）なのでhistoryから取得は難しいか？
-    #       cookie_session のlanding_pageで良い？
-    #       catalog/staff 両方で必要
-    $c->flash( ref => $c->req->url->to_string ) if $c->is_get_request;
-
-    $c->redirect_to( $c->url_for('RN_customer_login') );
-    return 0;
-}
-
 sub remember_me_handler {
     my $c = shift;
     return 1 if $c->is_logged_in;
