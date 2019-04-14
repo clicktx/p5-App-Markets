e2b8b8d2450cf6e3ec4312b67ad1d0f3cf49976a
clicktx <clicktx@gmail.com>
2019-04-14 17:17:32 +0900

    add resultset Email

---
+++ lib/Yetie/Schema/ResultSet/Email.pm
@@ -0,0 +1,36 @@
+package Yetie::Schema::ResultSet::Email;
+use Mojo::Base 'Yetie::Schema::ResultSet';
+
+1;
+__END__
+=encoding utf8
+
+=head1 NAME
+
+Yetie::Schema::ResultSet::Email
+
+=head1 SYNOPSIS
+
+    my $result = $schema->resultset('Email')->method();
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Schema::ResultSet::Email> inherits all attributes from L<Yetie::Schema::ResultSet> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Schema::ResultSet::Email> inherits all methods from L<Yetie::Schema::ResultSet> and implements
+the following new ones.
+
+=head2 C<verified>
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Schema::ResultSet>, L<Yetie::Schema>
