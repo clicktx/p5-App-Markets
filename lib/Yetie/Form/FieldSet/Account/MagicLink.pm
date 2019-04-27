d4eb6242b1deb01fc34b79e88002f695a4319c64
clicktx <clicktx@gmail.com>
2019-04-27 10:27:17 +0900

    remove noneed form fieldset

--- lib/Yetie/Form/FieldSet/Account/MagicLink.pm
+++
@@ -1,9 +0,0 @@
-package Yetie::Form::FieldSet::Account::MagicLink;
-use Mojo::Base -strict;
-use Yetie::Form::FieldSet;
-
-has_field email => extends('account-login#email');
-
-has_field remember_me => extends('account-login#remember_me');
-
-1;
