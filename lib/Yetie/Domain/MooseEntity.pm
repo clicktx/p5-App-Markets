e480db514d008075a76e0f31888d088cd6b528dd
clicktx <clicktx@gmail.com>
2019-05-24 06:54:24 +0900

    implement entity moose base

---
+++ lib/Yetie/Domain/MooseEntity.pm
@@ -0,0 +1,291 @@
+package Yetie::Domain::MooseEntity;
+use Yetie::Factory;
+use Yetie::Domain::Collection qw();
+use Yetie::Domain::IxHash qw();
+use Mojo::Util qw();
+use Scalar::Util qw();
+use Data::Clone qw();
+use Data::Dumper;
+
+use Moose;
+extends 'Yetie::Domain::MooseBase';
+
+my @not_dump_attrs_defautls = (qw/created_at updated_at/);
+
+has _hash_sum => (
+    is         => 'ro',
+    isa        => 'Str',
+    lazy_build => 1,
+    writer     => '_set_hash_sum',
+);
+sub _build__hash_sum { return shift->hash_code }
+
+has id => ( is => 'rw' );
+
+sub BUILD {
+    my $self = shift;
+
+    # Lazy build
+    $self->_hash_sum;
+}
+
+sub clone {
+    my $self  = shift;
+    my $clone = Data::Clone::data_clone($self);
+
+    my @attributes = keys %{$self};
+    foreach my $attr (@attributes) {
+        next unless $self->can($attr);
+        next unless Scalar::Util::blessed( $self->$attr );
+        $clone->$attr( $self->$attr->map( sub { $_->clone } ) ) if $self->$attr->can('map');
+    }
+
+    # Reset object hash_sum
+    $clone->_set_hash_sum( $clone->hash_code );
+    return $clone;
+}
+
+sub equals {
+    my ( $self, $obj ) = @_;
+    return $self->hash_code eq $obj->hash_code ? 1 : 0;
+}
+
+sub factory { Yetie::Factory->new( $_[1] ) }
+
+sub has_id { return shift->id ? 1 : 0 }
+
+sub hash_code {
+    my ( $self, $arg ) = @_;
+    return Mojo::Util::sha1_sum($arg) if $arg;
+
+    return Mojo::Util::sha1_sum( shift->_dump_public_attr );
+}
+
+sub is_empty { shift->id ? 0 : 1 }
+
+sub is_modified {
+    my $self = shift;
+    return 1 if $self->_hash_sum ne $self->hash_code;
+
+    # Recursive call for attributes
+    return $self->_recursive_call( sub { return 1 if shift->is_modified } ) ? 1 : 0;
+}
+
+sub reset_modified { warn 'reset_modified() is deprecated' }
+
+sub to_array {
+    my $self = shift;
+    my $hash = $self->to_hash;
+
+    my @keys = sort keys %{$hash};
+    my @values = map { $hash->{$_} } @keys;
+    return [ \@keys, \@values ];
+}
+
+sub to_data {
+    my $self = shift;
+    my $hash = $self->to_hash;
+    my %data;
+    foreach my $key ( keys %{$hash} ) {
+        my $value = $hash->{$key};
+        if ( Scalar::Util::blessed $value) { $data{$key} = $value->to_data if $value->can('to_data') }
+        else                               { $data{$key} = $value }
+    }
+    return \%data;
+}
+
+sub to_hash {
+    my %hash = %{ +shift };
+
+    # Remove needless data
+    my @not_dump = ();
+    push @not_dump, @not_dump_attrs_defautls, grep { /\A_.*/sxm } keys %hash;
+    foreach (@not_dump) { delete $hash{$_} }
+    return \%hash;
+}
+
+sub _dump_public_attr {
+    local $Data::Dumper::Terse    = 1;
+    local $Data::Dumper::Indent   = 0;
+    local $Data::Dumper::Sortkeys = sub {
+        my ($hash) = @_;
+        my @keys = grep { /\A(?!_).*/sxm } ( sort keys %{$hash} );
+        return \@keys;
+    };
+    return Dumper(shift);
+}
+
+sub _recursive_call {
+    my ( $self, $cb ) = @_;
+    foreach my $attr ( keys %{$self} ) {
+        next if !$self->can($attr);
+        next if !Scalar::Util::blessed( $self->$attr );
+
+        if ( $self->$attr->isa('Yetie::Domain::Entity') ) {
+            $cb->( $self->$attr );
+        }
+        elsif ( $self->$attr->isa('Yetie::Domain::Collection') ) {
+            $self->$attr->each( sub { $cb->($_) } );
+        }
+        elsif ( $self->$attr->isa('Yetie::Domain::IxHash') ) {
+            $self->$attr->each( sub { $cb->($b) } );
+        }
+    }
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Yetie::Domain::Entity - Entity Object Base Class
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head2 C<collection>
+
+    my $collection = collection( 'foo', 'bar', 'baz' );
+
+Construct a new index-hash-based L<Yetie::Domain::Collection> object.
+
+=head2 C<ixhash>
+
+    my $ixhash = ixhash( foo => 1, bar => 2, baz => 3 );
+
+Construct a new index-hash-based L<Yetie::Domain::IxHash> object.
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Entity> inherits all attributes from L<Yetie::Domain::Base> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Domain::Entity> inherits all methods from L<Yetie::Domain::Base> and implements
+the following new ones.
+
+=head2 C<clone>
+
+    my $clone = $self->clone;
+
+Return object.
+
+=head2 C<equals>
+
+    my $bool = $entity->equals($other_entity);
+
+Return boolean value.
+
+=head2 C<factory>
+
+    __PACKAGE__->factory('entity-foo');
+
+Return Yetie::Factory object.
+
+=head2 C<has_id>
+
+    my $bool = $entity->has_id;
+
+Return boolean value.
+
+=head2 C<hash_code>
+
+    my $sha1_sum = $entity->hash_code;
+    my $sha1_sum = $entity->hash_code($bytes);
+
+Return SHA1 checksum. Default bytes is L<Yetie::Domain::Entity/id>.
+
+=head2 C<id>
+
+    my $entity_id = $entity->id;
+
+=head2 C<is_empty>
+
+    my $bool = $entity->is_empty;
+
+Return boolean value.
+
+=head2 C<is_modified>
+
+    # Getter
+    my $bool = $entity->is_modified;
+    say 'Entity is modified!' if $bool;
+
+    # Setter
+    $entity->is_modified($bool);
+
+Return boolean value.
+
+=head2 C<reset_modified>
+
+    $entity->reset_modified;
+
+Reset modified flag.
+Recursively reset all entities to keep.
+
+=head2 C<to_array>
+
+    my $array = $entity->to_array;
+
+    # eg.
+    my $e = Yetie::Domain::Entity->new({ id => 11, b => 5, a => 3, z => 15 });
+    my $array = $e->to_array;
+
+    # Return value
+    print Dumper $array;
+    $VAR1 = [
+        [ 'a', 'b', 'z' ],
+        [ 3, 5, 15 ]
+    ];
+
+Return Array reference.
+
+Note: All private attributes (e.g. "_xxx") and "id", "created_at", "updated_at" are removed.
+
+=head2 C<to_data>
+
+    my $data = $e->to_data;
+
+Return hash reference. This method recursive call L</to_hash>.
+
+Note: All private attributes (e.g. "_xxx") and "id", "created_at", "updated_at" are removed.
+
+Objects that do not have method "to_data" are also deleted.
+
+    my $e = Yetie::Domain::Entity->new({
+        a => 1,
+        b => Yetie::Domain::Collection->new(1,2,3);
+        u => Mojo::URL->new,    # do not have method "to_data"
+    });
+    my $data = $e->to_data;
+
+    print Dumper $data;
+    $VAR1 = {
+        a => 1,
+        b => [1,2,3],
+    };
+
+=head2 C<to_hash>
+
+    my $data = $entity->to_hash;
+
+Return Hash reference.
+
+Note: All private attributes (e.g. "_xxx") and "id", "created_at", "updated_at" are removed.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Domain::Base>, L<Moose>, L<Yetie::Domain::Collection>, L<Yetie::Domain::IxHash>
---
+++ t/domain/entity_moose.t
@@ -0,0 +1,223 @@
+use Mojo::Base -strict;
+use Test::More;
+use Test::Deep;
+use Mojo::Util qw/sha1_sum/;
+use Yetie::Domain::Collection qw/collection/;
+use Yetie::Domain::IxHash qw/ixhash/;
+use Yetie::Domain::Value;
+use Mojo::URL;
+
+my $pkg = 'Yetie::Domain::MooseEntity';
+use_ok $pkg;
+
+{
+
+    package t::entity::foo;
+    use Moose;
+    extends 'Yetie::Domain::MooseEntity';
+
+    has [qw(hoge fuga)] => ( is => 'rw' );
+
+    package t::entity::bar;
+    use Moose;
+    extends 'Yetie::Domain::MooseEntity';
+
+    has [qw(hoge fuga)] => ( is => 'rw' );
+}
+
+subtest 'basic' => sub {
+    my $e = t::entity::foo->new( id => 1, hoge => 1, fuga => 2 );
+    cmp_deeply $e->to_hash, { id => 1, hoge => 1, fuga => 2 }, 'right to_hash';
+
+    $e->id(1);
+    is $e->is_modified, 0, 'right not modified of update attribute but no change value';
+    $e->id(111);
+    is $e->is_modified, 1, 'right modified of update attribute';
+
+    $e = t::entity::foo->new( id => 1, hoge => 1, fuga => 2 );
+    $e->hoge(111);
+    is $e->is_modified, 1, 'right modified of update attribute';
+};
+
+subtest 'hash_code' => sub {
+    my $e = $pkg->new( id => 1 );
+    is $e->hash_code, '6fb2e88e2a0a6400b34ba5a0e8586f099203d1b2', 'right hash code';
+    is $e->hash_code(2), sha1_sum(2), 'right hash code';
+};
+
+subtest 'factory' => sub {
+    my $e = $pkg->new();
+    my $f = $pkg->factory('entity-foo');
+    isa_ok $f, 'Yetie::Factory';
+};
+
+subtest 'function' => sub {
+    my $c = collection( 1, 2, 3 );
+    isa_ok $c, 'Yetie::Domain::Collection';
+    is_deeply $c, [ 1, 2, 3 ], 'right deeply';
+
+    my $h = ixhash( a => 1, b => 2 );
+    isa_ok $h, 'Yetie::Domain::IxHash';
+    is_deeply $h, { a => 1, b => 2 }, 'right deeply';
+};
+
+subtest 'clone' => sub {
+    my $data = { id => 1, hoge => [ {}, {} ], fuga => { a => [ {}, {} ] } };
+    my $e = t::entity::foo->new($data);
+    $e->id(2);
+
+    my $clone = $e->clone;
+    is $clone->is_modified, 0, 'right modified flag';
+
+    isnt $e, $clone, 'right clone';
+    cmp_deeply $e->to_hash, $clone->to_hash, 'right clone data structure';
+
+    isnt $e->{hoge}, $clone->{hoge}, 'right another reference';
+    $e->{hoge}->[0] = '';
+    cmp_deeply $clone->{hoge}->[0], +{}, 'right two dimensions';
+    $e->{fuga}->{a}->[0] = { b => 1 };
+    cmp_deeply $clone->{fuga}->{a}->[0], {}, 'right three dimensions';
+};
+
+subtest 'has_id' => sub {
+    my $e = t::entity::foo->new( id => 1 );
+    ok $e->has_id, 'right has data';
+
+    $e = t::entity::foo->new();
+    ok !$e->has_id, 'right has not data';
+};
+
+subtest 'is_empty' => sub {
+    my $e = t::entity::foo->new( id => 1 );
+    ok !$e->is_empty, 'right is not empty';
+
+    $e = t::entity::foo->new();
+    ok $e->is_empty, 'right is empty';
+};
+
+subtest 'to_array method' => sub {
+    my $data = { id => 1, hoge => 1, fuga => 2 };
+    my $e = t::entity::foo->new($data);
+    cmp_deeply $e->to_array, [ [qw/fuga hoge id/], [qw/2 1 1/] ], 'right to_array';
+};
+
+subtest 'Entity object base' => sub {
+    my $e1   = t::entity::foo->new( id => 1 );
+    my $e1_1 = t::entity::foo->new( id => 1 );
+    my $e2   = t::entity::bar->new( id => 2 );
+
+    is $e1->id, 1, 'right entity id';
+    is $e1->equals($e1_1), 1, 'right equals object';
+    is $e1->equals($e2),   0, 'right not equals object';
+    is $e1->hash_code, 'c03af602dd7314097b2b90227c92e262b8e913a8', 'right hash code';
+
+    $e1->id(1);
+    is $e1->is_modified, 0, 'right not modified';
+    $e1->id(111);
+    is $e1->is_modified, 1, 'right modified';
+
+    my $obj = t::entity::foo->new;
+    is $obj->id, undef, 'right no data entity';
+};
+
+subtest 'to_data method' => sub {
+    {
+
+        package t::entity::to_data;
+        use Moose;
+        extends 'Yetie::Domain::MooseEntity';
+
+        has [qw(a b h1 h2 h3 url v)] => ( is => 'rw' );
+    }
+    my $e = t::entity::to_data->new(
+        a  => 1,
+        b  => 2,
+        h2 => t::entity::to_data->new(),
+        h3 => t::entity::to_data->new(
+            a  => 1,
+            h1 => t::entity::to_data->new( a => 1, b => 2 ),
+        ),
+        url => Mojo::URL->new,                                # has not "to_data" method.
+        v   => Yetie::Domain::Value->new( value => 'foo' ),
+    );
+
+    cmp_deeply $e->to_data,
+      {
+        a  => 1,
+        b  => 2,
+        h2 => {},
+        h3 => {
+            a  => 1,
+            h1 => {
+                a => 1,
+                b => 2
+            },
+        },
+        v => 'foo',
+      },
+      'right dump data';
+};
+
+subtest 'is_modified' => sub {
+    {
+
+        package t::entity::modified;
+        use Moose;
+        extends 'Yetie::Domain::MooseEntity';
+
+        has [qw(a b c d e f g x y z)] => ( is => 'rw' );
+    }
+    my $make_entity = sub {
+        my $e = t::entity::modified->new(
+            a => 1,
+            b => 1,
+            e => t::entity::modified->new( x => 1 ),
+            c => collection(
+                t::entity::modified->new( y => 1 ),
+                t::entity::modified->new( e => t::entity::modified->new( z => 1 ) )
+            ),
+            d => ixhash( aa => t::entity::modified->new( f => 1, g => 1 ) ),
+        );
+        return $e;
+    };
+
+    my $e = $make_entity->();
+    is $e->is_modified, 0;
+    $e->{b} = 2;    # bad setter
+    is $e->is_modified, 1;
+
+    $e = $make_entity->();
+    $e->a(2);
+    is $e->is_modified, 1;
+
+    # Entity
+    subtest 'has entity' => sub {
+        $e = $make_entity->();
+        $e->e->x(2);
+        is $e->is_modified, 1;
+    };
+
+    # Collection
+    subtest 'has collection' => sub {
+        $e = $make_entity->();
+        $e->c->[0]->y(2);
+        is $e->is_modified, 1;
+
+        $e = $make_entity->();
+        $e->c->[1]->e->z(2);
+        is $e->is_modified, 1;
+    };
+
+    # IxHash
+    subtest 'has IxHash' => sub {
+        $e = $make_entity->();
+        $e->d->aa->f(2);
+        is $e->is_modified, 1;
+
+        $e = $make_entity->();
+        $e->d->aa->g(2);
+        is $e->is_modified, 1;
+    };
+};
+
+done_testing();
