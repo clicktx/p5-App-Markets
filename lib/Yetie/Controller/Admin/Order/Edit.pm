4d5e7e66ff994361cbae5f3898daf0a808fb82cc
clicktx <clicktx@gmail.com>
2019-06-06 08:53:42 +0900

    improve admin order edit address

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -33,7 +33,7 @@ sub items {
 # また、管理者注文も考慮する必要がある。
 sub billing_address {
     my $c = shift;
-    $c->_edit_address(
+    return $c->_edit_address(
         {
             address_type => 'billing_address',
             title        => 'Edit Billing Address',
@@ -43,7 +43,7 @@ sub billing_address {
 
 sub shipping_address {
     my $c = shift;
-    $c->_edit_address(
+    return $c->_edit_address(
         {
             address_type => 'shipping_address',
             title        => 'Edit Shipping Address',
@@ -57,25 +57,30 @@ sub _edit_address {
 
     my $order_id = $c->stash('id');
     my $order    = $c->service('order')->find_order($order_id);
-    $order->page_title( $order->page_title . '/' . $args->{title} );
-    $c->stash( entity => $order );
     return $c->reply->not_found if $order->is_empty;
 
     # Init form
     my $address = $order->{ $args->{address_type} };
     my $form    = $c->_init_form($address);
 
+    # Page
+    my $page = $c->factory('entity-page')->construct(
+        title => $args->{title},
+        form  => $form,
+    );
+
+    # Stash
+    $c->stash( page => $page, order => $order );
+    $c->template('admin/order/edit/address');
+
     # Get request
-    return $c->render('admin/order/edit/address') if $c->is_get_request;
+    return $c->render() if $c->is_get_request;
 
     # Validation form
-    return $c->render('admin/order/edit/address') unless $form->do_validate;
+    return $c->render() if !$form->do_validate;
 
     # Update address
-    # FIXME: 入力済み項目を消す事ができない。
-    # $form->params->to_hash が空文字を出力しないため例えば会社名を消す事ができない
     $c->service('address')->update_address( $form->params->to_hash );
-
     return $c->redirect_to( 'rn.admin.order.index', order_id => $order_id );
 }
 
