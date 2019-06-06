67b00a291ecd1d8746390f5fe59455c49755bb43
clicktx <clicktx@gmail.com>
2019-06-06 17:53:26 +0900

    improve page admin/page/order edit items

--- lib/Yetie/Controller/Admin/Order/Edit.pm
+++ lib/Yetie/Controller/Admin/Order/Edit.pm
@@ -1,34 +1,6 @@
 package Yetie::Controller::Admin::Order::Edit;
 use Mojo::Base 'Yetie::Controller::Admin';
 
-sub items {
-    my $c = shift;
-
-    my $order_id = $c->stash('id');
-    my $order    = $c->service('order')->find_order($order_id);
-    $order->page_title( $order->page_title . '/' . 'Edit Items' );
-    $c->stash( entity => $order );
-    return $c->reply->not_found if $order->is_empty;
-
-    my $form = $c->form('admin-order-edit-item');
-
-    # Get request
-    return $c->render() if $c->is_get_request;
-
-    # Validation form
-    return $c->render() unless $form->do_validate;
-
-    # Update
-    my $params = $form->param('item') || {};
-    foreach my $key ( keys %{$params} ) {
-        my $id = $key;
-        $id =~ s/\*//g;
-        my $values = $params->{$key};
-        $c->resultset('Sales::Order::Item')->search( { id => $id } )->update($values);
-    }
-    return $c->redirect_to( 'rn.admin.order.index', order_id => $order_id );
-}
-
 # NOTE: Catalog::Checkoutに関連する実装がある。
 # また、管理者注文も考慮する必要がある。
 sub billing_address {
@@ -39,6 +11,45 @@ sub billing_address {
             title        => 'Edit Billing Address',
         }
     );
+}
+
+sub items {
+    my $c = shift;
+
+    my $order_id = $c->stash('id');
+    my $order    = $c->service('order')->find_order($order_id);
+    return $c->reply->not_found if $order->is_empty;
+
+    # Init form
+    my $form = $c->form('admin-order-edit-item');
+
+    # Page
+    my $page = $c->factory('entity-page')->construct(
+        title => 'Edit Items',
+        form  => $form,
+    );
+
+    # Stash
+    $c->stash( page => $page, order => $order );
+
+    # Get request
+    return $c->render() if $c->is_get_request;
+
+    # Validation form
+    return $c->render() if !$form->do_validate;
+
+    # FIXME:
+    # Update
+    my $params = $form->param('item') || {};
+    foreach my $key ( keys %{$params} ) {
+        my $id = $key;
+        $id =~ s/\*//g;
+        my $values = $params->{$key};
+        $c->resultset('Sales::Order::Item')->search( { id => $id } )->update($values);
+    }
+
+    my $url = $c->url_for( 'rn.admin.order.index', order_id => $order_id )->fragment('items');
+    return $c->redirect_to($url);
 }
 
 sub shipping_address {
@@ -81,7 +92,10 @@ sub _edit_address {
 
     # Update address
     $c->service('address')->update_address( $form->params->to_hash );
-    return $c->redirect_to( 'rn.admin.order.index', order_id => $order_id );
+
+    my $url = $c->url_for( 'rn.admin.order.index', order_id => $order_id )->fragment( $c->stash('action') );
+    return $c->redirect_to($url);
+
 }
 
 sub _init_form {
