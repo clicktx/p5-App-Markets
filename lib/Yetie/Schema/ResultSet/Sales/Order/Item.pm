e8a917c4df0ecb313de2bc0380423d544e6f64eb
clicktx <clicktx@gmail.com>
2019-06-07 19:17:19 +0900

    implement method store_items

--- lib/Yetie/Schema/ResultSet/Sales/Order/Item.pm
+++ lib/Yetie/Schema/ResultSet/Sales/Order/Item.pm
@@ -1,6 +1,21 @@
 package Yetie::Schema::ResultSet::Sales::Order::Item;
 use Mojo::Base 'Yetie::Schema::ResultSet';
 
+sub store_items {
+    my ( $self, $order ) = @_;
+
+    my $order_id = $order->id;
+    my $items    = $order->items;
+    $items->each(
+        sub {
+            my $item = shift;
+            my %data = ( order_id => $order_id, %{ $item->to_data } );
+            $self->update_or_create( \%data );
+        }
+    );
+    return $self;
+}
+
 1;
 __END__
 =encoding utf8
@@ -24,6 +39,14 @@ the following new ones.
 
 L<Yetie::Schema::ResultSet::Sales::Order::Item> inherits all methods from L<Yetie::Schema::ResultSet> and implements
 the following new ones.
+
+=head2 C<store_items>
+
+Create or update items.
+
+    $rs->store_items( $order );
+
+Argument: L<Yetie::Domain::Entity::OrderDetail> object.
 
 =head1 AUTHOR
 
