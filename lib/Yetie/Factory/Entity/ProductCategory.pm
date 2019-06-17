f264c3d7ebf10ed17323fe7d461861768ac165bb
clicktx <clicktx@gmail.com>
2019-06-17 09:34:16 +0900

    add factory for product_category

---
+++ lib/Yetie/Factory/Entity/ProductCategory.pm
@@ -0,0 +1,43 @@
+package Yetie::Factory::Entity::ProductCategory;
+use Mojo::Base 'Yetie::Factory';
+
+sub cook {
+    my $self = shift;
+
+    # children
+    $self->aggregate( ancestors => 'list-category_ancestors', $self->param('ancestors') || [] );
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Factory::Entity::ProductCategory
+
+=head1 SYNOPSIS
+
+    my $entity = Yetie::Factory::Entity::ProductCategory->new( %args )->construct();
+
+    # In controller
+    my $entity = $c->factory('entity-product_category')->construct(%args);
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Factory::Entity::ProductCategory> inherits all attributes from L<Yetie::Factory> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Factory::Entity::ProductCategory> inherits all methods from L<Yetie::Factory> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Factory>
