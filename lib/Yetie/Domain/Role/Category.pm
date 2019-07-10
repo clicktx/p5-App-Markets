78ab53ac1a352773633ee8ef2d1ac47c960a4f70
clicktx <clicktx@gmail.com>
2019-07-10 09:23:04 +0900

    add role

---
+++ lib/Yetie/Domain/Role/Category.pm
@@ -0,0 +1,47 @@
+package Yetie::Domain::Role::Category;
+use Moose::Role;
+
+has level    => ( is => 'ro', default => 0 );
+has lft      => ( is => 'ro', default => 0 );
+has rgt      => ( is => 'ro', default => 0 );
+has root_id  => ( is => 'ro', default => 0 );
+has title    => ( is => 'ro', default => q{} );
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Domain::Role::Category
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Domain::Role::Category> inherits all attributes from L<Moose::Role> and implements
+the following new ones.
+
+=head2 C<level>
+
+=head2 C<lft>
+
+=head2 C<rgt>
+
+=head2 C<root_id>
+
+=head2 C<title>
+
+=head1 METHODS
+
+L<Yetie::Domain::Role::Category> inherits all methods from L<Moose::Role> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Moose::Role>
