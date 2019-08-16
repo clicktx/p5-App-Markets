0911eea84bd308e0a3ef4340302a5c60ac1dc605
clicktx <clicktx@gmail.com>
2019-08-16 18:54:15 +0900

    implement method and attribute

--- lib/Yetie/Domain/Value/Price.pm
+++ lib/Yetie/Domain/Value/Price.pm
@@ -1,10 +1,20 @@
 package Yetie::Domain::Value::Price;
 use MooseX::Types::Common::Numeric qw/PositiveNum/;
+use Math::Currency;
+use overload q{""} => sub { $_[0]->amount }, fallback => 1;
 
 use Moose;
 extends 'Yetie::Domain::Value';
 
 has '+value' => ( isa => PositiveNum );
+
+has is_tax_included => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+sub amount { Math::Currency->new( shift->value ) }
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
@@ -25,10 +35,32 @@ Yetie::Domain::Value::Price
 L<Yetie::Domain::Value::Price> inherits all attributes from L<Yetie::Domain::Value> and implements
 the following new ones.
 
+=head2 C<value>
+
+PositiveNum only.
+
+=head2 C<is_tax_included>
+
+Return boolean value.
+
 =head1 METHODS
 
 L<Yetie::Domain::Value::Price> inherits all methods from L<Yetie::Domain::Value> and implements
 the following new ones.
+
+=head2 C<amount>
+
+Return L<Math::Currency> object.
+
+=head1 OPERATOR
+
+L<Yetie::Domain::Value::Price> overloads the following operators.
+
+=head2 C<stringify>
+
+    my $amount = "$price";
+
+Alias for L</amount>.
 
 =head1 AUTHOR
 
--- t/domain/value/price.t
+++ t/domain/value/price.t
@@ -12,4 +12,19 @@ subtest 'basic' => sub {
     lives_ok { $pkg->new( value => 0.1 ) } 'right lives';
 };
 
+subtest 'amount' => sub {
+    Math::Currency->format('USD');
+    my $price = $pkg->new(100);
+    is $price->amount, '$100.00', 'right amount';
+    is $price, '$100.00', 'right operator';
+};
+
+subtest 'is_tax_included' => sub {
+    my $price = $pkg->new(1);
+    ok !$price->is_tax_included, 'right default';
+
+    $price = $pkg->new( value => 1, is_tax_included => 1 );
+    ok $price->is_tax_included, 'right tax included';
+};
+
 done_testing();
