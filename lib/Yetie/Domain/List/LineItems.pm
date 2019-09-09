70069e5a91894305824b5d1c7ea047306bdd9600
clicktx <clicktx@gmail.com>
2019-09-09 18:03:17 +0900

    remove method total_amount

--- lib/Yetie/Domain/List/LineItems.pm
+++ lib/Yetie/Domain/List/LineItems.pm
@@ -76,15 +76,6 @@ sub remove {
     return 1;
 }
 
-sub total_amount {
-    my $self = shift;
-    warn '!!DEPRECATED!!';
-
-    my $total_amount = 0;
-    $self->each( sub { $total_amount += $_->subtotal } );
-    return $total_amount;
-}
-
 sub _append_item {
     my ( $self, $item ) = @_;
     my $new = $self->list->append($item);
@@ -156,12 +147,6 @@ the following new ones.
     $items->remove($line_num);
 
 Return true if remove item.
-
-=head2 C<total_amount>
-
-DEPRECATED
-
-    my $total_amount = $items->total_amount;
 
 =head1 AUTHOR
 
