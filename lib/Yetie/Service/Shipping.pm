c9097dd4b929258d78218af8385625c3736f67df
clicktx <clicktx@gmail.com>
2019-10-30 20:14:43 +0900

    add service Shipping

---
+++ lib/Yetie/Service/Shipping.pm
@@ -0,0 +1,31 @@
+package Yetie::Service::Shipping;
+use Mojo::Base 'Yetie::Service';
+
+1;
+__END__
+
+=head1 NAME
+
+Yetie::Service::Shipping
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+L<Yetie::Service::Shipping> inherits all attributes from L<Yetie::Service> and implements
+the following new ones.
+
+=head1 METHODS
+
+L<Yetie::Service::Shipping> inherits all methods from L<Yetie::Service> and implements
+the following new ones.
+
+=head1 AUTHOR
+
+Yetie authors.
+
+=head1 SEE ALSO
+
+L<Yetie::Service>
