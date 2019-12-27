d2e562f80a2d7d2f07b06c3bdae628c2707e3249
clicktx <clicktx@gmail.com>
2019-12-27 20:10:39 +0900

    implement method to_form_choices

--- lib/Yetie/Domain/List/PaymentMethods.pm
+++ lib/Yetie/Domain/List/PaymentMethods.pm
@@ -3,6 +3,10 @@ use Moose;
 use namespace::autoclean;
 extends 'Yetie::Domain::List';
 
+sub to_form_choices {
+    return shift->reduce( sub { [ @{$a}, [ $b->name, $b->id ] ] }, [] );
+}
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 
@@ -26,6 +30,18 @@ the following new ones.
 
 L<Yetie::Domain::List::PaymentMethods> inherits all methods from L<Yetie::Domain::List> and implements
 the following new ones.
+
+=head2 C<to_form_choices>
+
+    # [ [ foo => 1 ], [ bar => 2 ], ... ]
+    my $form_choices = $list->to_form_choices();
+
+    # set choices
+    $form->field('field_name')->choices($form_choices);
+
+Return array reference of array reference.
+
+See L<Yetie::Form::Field/choices>
 
 =head1 AUTHOR
 
