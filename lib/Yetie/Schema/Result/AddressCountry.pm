c007a343894ddb11bf83461408b78d1dfe34183c
clicktx <clicktx@gmail.com>
2020-03-22 11:32:31 +0900

    add schema address country region

--- lib/Yetie/Schema/Result/AddressCountry.pm
+++ lib/Yetie/Schema/Result/AddressCountry.pm
@@ -8,10 +8,29 @@ primary_column code => {
     comments  => 'ISO 3166-1 alpha-2',
 };
 
+# AS EU NA SA AU
+# column continent => {
+#     data_type   => 'VARCHAR',
+#     size        => 2,
+#     is_nullable => 0,
+# };
+
 column name => {
     data_type   => 'VARCHAR',
     size        => 64,
     is_nullable => 0,
 };
+
+column position => {
+    data_type     => 'INT',
+    default_value => 100,
+    is_nullable   => 0,
+};
+
+# Relation
+has_many
+  regions => 'Yetie::Schema::Result::AddressCountryRegion',
+  { 'foreign.country_code' => 'self.code' },
+  { cascade_delete       => 0 };
 
 1;
---
+++ lib/Yetie/Schema/Result/AddressCountryRegion.pm
@@ -0,0 +1,56 @@
+package Yetie::Schema::Result::AddressCountryRegion;
+use Mojo::Base 'Yetie::Schema::Result';
+use DBIx::Class::Candy -autotable => v1;
+
+use Yetie::Schema::Result::AddressCountry;
+
+primary_column id => {
+    data_type         => 'INT',
+    is_auto_increment => 1,
+};
+
+column country_code => {
+
+    %{ Yetie::Schema::Result::AddressCountry->column_info('code') },
+    is_nullable => 0,
+};
+
+column code => {
+    data_type => 'VARCHAR',
+    size      => 8,
+    comments  => 'ISO 3166-2',
+};
+
+column name => {
+    data_type   => 'VARCHAR',
+    size        => 64,
+    is_nullable => 0,
+};
+
+# column postal_regex => {
+#     data_type   => 'VARCHAR',
+#     size        => 64,
+#     is_nullable => 1,
+# };
+
+# NOTE: 緯度経度を扱うにはMySQL5.7以上が推奨？
+# latitude
+# longitude
+
+column position => {
+    data_type     => 'INT',
+    default_value => 100,
+    is_nullable   => 0,
+};
+
+# Relation
+belongs_to
+  country => 'Yetie::Schema::Result::AddressCountry',
+  { 'foreign.code' => 'self.country_code' };
+
+has_many
+  shipping_zones => 'Yetie::Schema::Result::ShippingCarrierServiceZoneRegion',
+  { 'foreign.region_code' => 'self.code' },
+  { cascade_delete             => 0 };
+
+1;
