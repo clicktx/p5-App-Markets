28e6b586edf1256772b69cbf7f2d6f8a4ae7a090
clicktx <clicktx@gmail.com>
2020-04-06 19:47:18 +0900

    add state in US

--- share/sample_data.pl
+++ share/sample_data.pl
@@ -210,12 +210,12 @@
         [ 7, 'g@example.org', 0 ],
     ],
     'Address' => [
-        [qw/id country_code line1 line2 city state postal_code personal_name organization phone hash/],
-        [ 1, 'US', '42 Pendergast St.', '', 'Piedmont', 'SC', '29673', 'Elizabeth T. Peoples', 'Eli Moore Inc', '3059398498', '8ebc3d079a37d641bbff9cee147a00a0d3e24f64' ],
-        [ 2, 'US', '7004 Tunnel St.', '', 'New Brunswick', 'NJ', '08901', 'Craig M. Hise', 'Big Apple', '8037671849', '6f35ab273ec74992ad4a0eef48068ac063e82705' ],
-        [ 3, 'US', '803 Wild Rose St.', '', 'Woodstock', 'GA', '30188', 'Mary R. Johnson', '', '8047441468', 'ddd5a06df33a7a7f72d5c80284df6f3d6cecab65' ],
-        [ 4, 'US', '906 Pearl Ave.', '', 'Patchogue', 'NY', '11772', 'Albert B. Hastings', 'Twin Food Stores', '7089871400', '3b0af55dc218a878c041a6cb3f3467daddf6ead8' ],
-        [ 5, 'JP', '北多久町 4-2-4', 'リバーサイド室瀬212', '多久市', '佐賀県', '8460001', '沢井 咲', '株式会社 フィーデザイン', '09054172962', '7147e4227a0ced37c758a93c6f7fcfe6f34ff584' ],
+        [qw/id country_code state_id line1 line2 city state postal_code personal_name organization phone hash/],
+        [ 1, 'US', '92', '42 Pendergast St.', '', 'Piedmont', 'SC', '29673', 'Elizabeth T. Peoples', 'Eli Moore Inc', '3059398498', '8ebc3d079a37d641bbff9cee147a00a0d3e24f64' ],
+        [ 2, 'US', '82', '7004 Tunnel St.', '', 'New Brunswick', 'NJ', '08901', 'Craig M. Hise', 'Big Apple', '8037671849', '6f35ab273ec74992ad4a0eef48068ac063e82705' ],
+        [ 3, 'US', '59', '803 Wild Rose St.', '', 'Woodstock', 'GA', '30188', 'Mary R. Johnson', '', '8047441468', 'ddd5a06df33a7a7f72d5c80284df6f3d6cecab65' ],
+        [ 4, 'US', '85', '906 Pearl Ave.', '', 'Patchogue', 'NY', '11772', 'Albert B. Hastings', 'Twin Food Stores', '7089871400', '3b0af55dc218a878c041a6cb3f3467daddf6ead8' ],
+        [ 5, 'JP', '41', '北多久町 4-2-4', 'リバーサイド室瀬212', '多久市', '佐賀県', '8460001', '沢井 咲', '株式会社 フィーデザイン', '09054172962', '7147e4227a0ced37c758a93c6f7fcfe6f34ff584' ],
     ],
 
     # Password
